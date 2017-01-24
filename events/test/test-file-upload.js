d8df1c2212fb5bde3457206a9898edb16f5b3a00
serge1peshcoff <serge1peshcoff@mail.ru>
2017-01-24 18:35:46 +0300

    Testing malformed upload requests

--- events/test/test-file-upload.js
+++ events/test/test-file-upload.js
@@ -28,6 +28,72 @@ describe('File upload', () => {
     fs.removeSync(config.media_dir);
   });
 
+  it('should create an upload folder if it doesn\'t exist', (done) => {
+    fs.removeSync(config.media_dir);
+
+    chai.request(server)
+      .post(`/single/${events[0]._id}/upload`)
+      .set('X-Auth-Token', 'foobar')
+      .attach('head_image', fs.readFileSync('./test/assets/valid_image.png'), 'image.png')
+      .end((err, res) => {
+        fs.existsSync(config.media_dir).should.be.true;
+        done();
+      });
+  });
+
+  it('should fail if the uploaded file is not an image (by extension)', (done) => {
+    chai.request(server)
+      .post(`/single/${events[0]._id}/upload`)
+      .set('X-Auth-Token', 'foobar')
+      .attach('head_image', fs.readFileSync('./test/assets/invalid_image.txt'), 'image.txt')
+      .end((err, res) => {
+        res.should.have.status(500);
+        res.should.be.json;
+        res.should.be.a('object');
+
+        res.body.success.should.be.false;
+        res.body.should.have.property('errors');
+        res.body.should.have.property('message');
+
+        done();
+      });
+  });
+
+  it('should fail if the uploaded file is not an image (by content)', (done) => {
+    chai.request(server)
+      .post(`/single/${events[0]._id}/upload`)
+      .set('X-Auth-Token', 'foobar')
+      .attach('head_image', fs.readFileSync('./test/assets/invalid_image.jpg'), 'image.jpg')
+      .end((err, res) => {
+        res.should.have.status(500);
+        res.should.be.json;
+        res.should.be.a('object');
+
+        res.body.success.should.be.false;
+        res.body.should.have.property('errors');
+        res.body.should.have.property('message');
+
+        done();
+      });
+  });
+
+  it('should fail the \'head_image\' field is not specified', (done) => {
+    chai.request(server)
+      .post(`/single/${events[0]._id}/upload`)
+      .set('X-Auth-Token', 'foobar')
+      .end((err, res) => {
+        res.should.have.status(500);
+        res.should.be.json;
+        res.should.be.a('object');
+
+        res.body.success.should.be.false;
+        res.body.should.have.property('errors');
+        res.body.should.have.property('message');
+
+        done();
+      });
+  });
+
   it('should upload a file if it\'s valid', (done) => {
     chai.request(server)
       .post(`/single/${events[0]._id}/upload`)
@@ -44,8 +110,4 @@ describe('File upload', () => {
         done();
       });
   });
-
-  it('should fail if the uploaded file is not an image (by extension)');
-  it('should fail if the uploaded file is not an image (by content)');
-  it('should create an upload folder if it doesn\'t exist');
 });
