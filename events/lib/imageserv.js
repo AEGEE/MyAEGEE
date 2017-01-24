6a3509fc704725f5e6e93c68da8ac2cf41eb323e
serge1peshcoff <serge1peshcoff@mail.ru>
2017-01-24 18:35:05 +0300

    Checking for file contents before upload

--- events/lib/imageserv.js
+++ events/lib/imageserv.js
@@ -1,10 +1,15 @@
 const restify = require('restify');
 const fs = require('fs-extra');
+const path = require('path');
 const multer = require('multer');
+const readChunk = require('read-chunk');
+const fileType = require('file-type');
+
 const log = require('./config/logger');
 const config = require('./config/config.js');
 
 const uploadFolderName = `${config.media_dir}/headimages`;
+const allowedExtensions = ['.png', '.jpg', '.jpeg'];
 
 const storage = multer.diskStorage({ // multers disk storage settings
   destination(req, file, cb) {
@@ -15,19 +20,66 @@ const storage = multer.diskStorage({ // multers disk storage settings
     cb(null, `${req.event.id}-${(new Date()).getTime()}`);
   },
 });
-const upload = multer({ storage }).single('head_image');
+const upload = multer({
+  storage,
+  fileFilter(req, file, cb) {
+    const extension = path.extname(file.originalname);
+    if (!allowedExtensions.includes(extension)) {
+      const allowed = allowedExtensions.map(e => `'${e}'`).join(', ');
+      return cb(new Error(`Allowed extensions: ${allowed}, but '${extension}' was passed.`));
+    }
 
-// If upload folder doesn't exists, create it.
-if (!fs.existsSync(uploadFolderName)) {
-  fs.mkdirpSync(uploadFolderName);
-}
+    return cb(null, true);
+  },
+}).single('head_image');
 
 exports.uploadImage = (req, res, next) => {
+  // If upload folder doesn't exists, create it.
+  if (!fs.existsSync(uploadFolderName)) {
+    fs.mkdirpSync(uploadFolderName);
+  }
+
   upload(req, res, (uploadErr) => {
     if (uploadErr) {
       log.error('Could not store image', uploadErr);
-      return next(new restify.InternalError());
+      return next(new restify.InternalError({
+        body: {
+          success: false,
+          errors: [uploadErr],
+          message: uploadErr.message,
+        },
+      }));
     }
+
+    // If the head_image field is missing, do nothing.
+    if (!req.file) {
+      return next(new restify.InternalError({
+        body: {
+          success: false,
+          errors: [new Error('No head_image is specified.')],
+          message: 'No head_image is specified.',
+        },
+      }));
+    }
+
+    // If the file's content is malformed, don't save it.
+    const buffer = readChunk.sync(req.file.path, 0, 4100);
+    const type = fileType(buffer);
+
+    const originalExtension = path.extname(req.file.originalname);
+    const determinedExtension = (type && type.ext ? `.${type.ext}` : 'unknown');
+
+    if (originalExtension !== determinedExtension
+      || !allowedExtensions.includes(determinedExtension)) {
+      return next(new restify.InternalError({
+        body: {
+          success: false,
+          errors: [new Error('Malformed file content.')],
+          message: 'Malformed file content.',
+        },
+      }));
+    }
+
 
     // If there was an old image, move that away later
     const oldimg = req.event.head_image;
@@ -39,7 +91,13 @@ exports.uploadImage = (req, res, next) => {
     return req.event.save((saveErr) => {
       if (saveErr) {
         log.error('Could not store image metadata to db', saveErr);
-        return next(new restify.InternalError());
+        return next(new restify.InternalError({
+          body: {
+            success: false,
+            errors: [saveErr],
+            message: saveErr.message,
+          },
+        }));
       }
 
       res.json({
@@ -50,8 +108,8 @@ exports.uploadImage = (req, res, next) => {
 
       // Move old file away
       if (oldimg && oldimg.path) {
-        const path = oldimg.path.split('/');
-        fs.rename(oldimg.path, `${config.media_dir}/old/${path[path.length - 1]}`, (err) => {
+        const oldPath = oldimg.path.split('/');
+        fs.rename(oldimg.path, `${config.media_dir}/old/${oldPath[path.length - 1]}`, (err) => {
           if (err) {
             log.warn('Could not move unused image into media/old folder', err);
           }
