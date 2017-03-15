5570c87c732112f11220c401c33f18ccf0deb9d8
serge1peshcoff <serge1peshcoff@mail.ru>
2017-03-15 22:35:02 +0300

    Make use of NODE_ENV

--- events/lib/config/config.js
+++ events/lib/config/config.js
@@ -1,10 +1,26 @@
 const config = require('./configFile.json');
 
-const exp = config.dev;
+// Assuming by default that we run in 'development' environment, if no
+// NODE_ENV is specified.
+process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+
+// Can't run without config.
+if (!config[process.env.NODE_ENV]) {
+  throw new Error(`No config for current environment: ${process.env.NODE_ENV}`);
+}
+
+// Using config for the current environment.
+const envConfig = config[process.env.NODE_ENV];
+
+// If the server is running in the test env, we just copy not filled in fields
+// from the 'development' env.
+// TODO: think about it.
 if (process.env.NODE_ENV === 'test') {
-  for (const attr in config.test) {
-    exp[attr] = config.test[attr];
+  for (const attr in config.development) {
+    if (!envConfig[attr]) {
+      envConfig[attr] = config.development[attr];
+    }
   }
 }
 
-module.exports = exp;
+module.exports = envConfig;
