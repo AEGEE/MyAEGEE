542191147da6e6dce2b29884a924f0b6f9e8a499
Derk Snijders - i3anaan <i3anaan@gmail.com>
2017-07-17 16:31:15 +0200

    Updated deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -5,13 +5,16 @@ cd oms-docker/docker
 docker-compose down -v
 cd ../../
 rm -Rf oms-docker
-echo "Finished cleanup"
+echo -e "\nFinished cleanup\n"
 git clone --recursive --branch dev https://github.com/AEGEE/oms-docker.git
 cp ./oms-core.env oms-docker/oms-core/example.env
 cp ./oms-core_UserSeeder.php oms-docker/oms-core/database/seeds/UserSeeder.php
-echo "Finished setting up files"
+echo -e "\nFinished setting up files\n"
 cd oms-docker/docker
 docker-compose up -d
-echo "System initializing..."
-echo "Exit logs by pressing ctrl+c, the server kees running"
+echo -e "\nInitializing..."
+echo -e "This might take a while"
+sleep 5
+echo -e "Logs will be shown, you can press ctrl+c at any time (without stopping the server)\n"
+sleep 10
 docker-compose logs -f
