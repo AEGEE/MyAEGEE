39e71ecf1fe9aae17f8e87a18ff59e46566addd7
Derk Snijders - i3anaan <i3anaan@gmail.com>
2017-07-26 21:35:39 +0200

    Disabled cache for building, removed container selection.

--- run.sh
+++ run.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-echo -e "USAGE: bash ./run.sh [--no-up | --build | [--down  --down_v] | -c=?]\n";
+echo -e "USAGE: bash ./run.sh [--no-up | --build | [--down  --down_v]]\n";
 
 # Default settings
 UP=YES
 BUILD=NO
 DOWN=NO
 DOWN_V=NO
-CONTAINER=
 
 for i in "$@"
 do
@@ -28,10 +27,6 @@ case $i in
     DOWN_V=YES
     shift # past argument=value
     ;;
-    -c=*)
-    CONTAINER="${i#*=}"
-    shift # past argument=value
-    ;;
     *)
             # unknown option
     ;;
@@ -43,18 +38,16 @@ echo -e "BUILD         (--build)       = ${BUILD}"
 echo -e "DOWN          (--down)        = ${DOWN}"
 echo -e "DOWN_V        (--down_v)      = ${DOWN_V}"
 
-echo -e "CONTAINER     (-c=?)          = ${CONTAINER}"
-
 
 
 docker network create OMS
 cd docker/
 [ "$DOWN" == "YES" ] && docker-compose down
 [ "$DOWN_V" == "YES" ] && docker-compose down -v
-[ "$BUILD" == "YES" ] && docker-compose build --force-rm --no-cache $CONTAINER
-[ "$UP" == "YES" ] && docker-compose up -d $CONTAINER
+[ "$BUILD" == "YES" ] && docker-compose build --force-rm --no-cache
+[ "$UP" == "YES" ] && docker-compose up -d
 cd ../oms-core/docker
 [ "$DOWN" == "YES" ] && docker-compose down
 [ "$DOWN_V" == "YES" ] && docker-compose down -v
-[ "$BUILD" == "YES" ] && docker-compose build --force-rm $CONTAINER
-[ "$UP" == "YES" ] && docker-compose up -d $CONTAINER
+[ "$BUILD" == "YES" ] && docker-compose build --force-rm --no-cache
+[ "$UP" == "YES" ] && docker-compose up -d
