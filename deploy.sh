14e7ad9bdb8edac6a5011f852c84bc7ea9e4d2de
Derk Snijders - i3anaan <i3anaan@gmail.com>
2017-08-16 21:16:10 +0200

    Updated deploy script.

--- deploy.sh
+++ deploy.sh
@@ -17,11 +17,7 @@ git clone --recursive --branch dev https://github.com/AEGEE/oms-docker.git
 cp ./oms-core.env oms-docker/oms-core/.env.example
 echo -e "\nFinished setting up files\n"
 
-cd oms-docker/docker
-docker-compose up -d
+cd oms-docker
 echo -e "\nInitializing..."
 echo -e "This might take a while"
-sleep 5
-echo -e "Logs will be shown, you can press ctrl+c at any time (without stopping the server)\n"
-sleep 10
-docker-compose logs -f
+bash ./oms.sh
