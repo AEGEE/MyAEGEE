e6d9a47fe21a4b26ceb0b46dd708d5f40b6efeaa
Derk Snijders - i3anaan <i3anaan@gmail.com>
2017-08-21 18:04:18 +0200

    Updated file location for LocalLoopbackTest.sh

--- tests/LocalLoopbackTest/LocalLoopbackTest.sh
+++ tests/LocalLoopbackTest/LocalLoopbackTest.sh
@@ -2,11 +2,13 @@
 
 #Ping OMS-core
 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
 sudo apt-get install python-setuptools
 sudo easy_install pip
 sudo pip install json-spec
 echo "Setup done."
-rm -f response.json
-curl -X POST -H "Content-Type:application/json" localhost/api/login > response.json
+rm -f $DIR/response.json
+curl -X POST -H "Content-Type:application/json" localhost/api/login > $DIR/response.json
 echo "Got response, checking..."
-json validate --schema-file=schema.json < response.json && echo "Valid response!"
+json validate --schema-file=$DIR/schema.json < $DIR/response.json && echo "Valid response!"
