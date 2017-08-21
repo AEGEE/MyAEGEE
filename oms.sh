8039322f168d417468880673b436e6e476ccb304
Derk Snijders - i3anaan <i3anaan@gmail.com>
2017-08-21 16:30:40 +0200

    Reworked oms.sh to work with -f option
    Also put the global/common OMS services into a oms-global folder
    Added a docker-compose environment file

--- oms.sh
+++ oms.sh
@@ -1,75 +1,25 @@
 #!/bin/bash
 
-echo -e "USAGE: bash ./oms.sh [--no-up | --build | [--down | --down_v] | --push]\n";
-
-# Default settings
-UP=YES
-BUILD=NO
-DOWN=NO
-DOWN_V=NO
-PUSH=NO
-
-#Parse input
-for i in "$@"
-do
-case $i in
-    --no-up)
-    UP=NO
-    shift # past argument=value
-    ;;
-    --build)
-    BUILD=YES
-    shift # past argument=value
-    ;;
-    --down)
-    DOWN=YES
-    shift # past argument=value
-    ;;
-    --down_v)
-    DOWN_V=YES
-    shift # past argument=value
-    ;;
-    --push)
-    PUSH=YES
-    shift # past argument=value
-    ;;
-    *)
-            # unknown option
-    ;;
-esac
-done
-
-#Display input
-echo -e "UP            (--no-up)       = ${UP}"
-echo -e "BUILD         (--build)       = ${BUILD}"
-echo -e "DOWN          (--down)        = ${DOWN}"
-echo -e "DOWN_V        (--down_v)      = ${DOWN_V}"
-echo -e "PUSH          (--push)        = ${PUSH}"
+## This file gathers all docker-compose.yml files from [subfolders]/docker/docker-compose.yml and merges them with the -f option of the compose command.
+## Instead of docker-compose up -d, write ./oms.sh up -d
 
 
-#
-## Script
-#
+if ! [[ -f ".env" ]]; then
+    echo -e "[OMS] Copying .env file from .env.example"
+    cp ./.env.example ./.env
+fi
 
-
-echo -e "OMS: Create network..."
-docker network create OMS
 
 ## Declare docker-compose.yml folders
-declare -a arr=("docker" "oms-core/docker")
+declare -a services=("oms-global" "oms-core")
 
-## Save base path
-BASE_PATH=$(pwd)
-
-## now loop through the above array
-for i in "${arr[@]}"
-do
-    echo -e "OMS: Process folder...  $i"
-    cd $i
-    [ "$DOWN" == "YES" ] && docker-compose down
-    [ "$DOWN_V" == "YES" ] && docker-compose down -v
-    [ "$BUILD" == "YES" ] && docker-compose build --force-rm --no-cache
-    [ "$PUSH" == "YES" ] && docker-compose push
-    [ "$UP" == "YES" ] && docker-compose up -d
-    cd $BASE_PATH
+command="docker-compose -f empty-docker-compose.yml"
+for s in "${services[@]}"; do
+    if [[ -f "${s}/docker/docker-compose.yml" ]]; then
+        command="${command} -f ${s}/docker/docker-compose.yml"
+    fi
 done
+
+command="${command} ${@}"
+echo -e "\n[OMS] Full command:\n${command}\n"
+eval $command
