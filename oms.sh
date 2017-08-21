73c7e48a530def6f17fbe00839fd01956b70207b
Derk Snijders - i3anaan <i3anaan@gmail.com>
2017-08-21 18:35:03 +0200

    Paths are now relative to script location instead of execution location

--- oms.sh
+++ oms.sh
@@ -3,10 +3,11 @@
 ## This file gathers all docker-compose.yml files from [subfolders]/docker/docker-compose.yml and merges them with the -f option of the compose command.
 ## Instead of docker-compose up -d, write ./oms.sh up -d
 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-if ! [[ -f ".env" ]]; then
+if ! [[ -f "$DIR/.env" ]]; then
     echo -e "[OMS] Copying .env file from .env.example"
-    cp ./.env.example ./.env
+    cp $DIR/.env.example $DIR/.env
 fi
 
 echo -e "[OMS] Creating OMS docker network"
@@ -18,8 +19,8 @@ declare -a services=("oms-global" "oms-core")
 
 command="docker-compose -f empty-docker-compose.yml"
 for s in "${services[@]}"; do
-    if [[ -f "${s}/docker/docker-compose.yml" ]]; then
-        command="${command} -f ${s}/docker/docker-compose.yml"
+    if [[ -f "$DIR/${s}/docker/docker-compose.yml" ]]; then
+        command="${command} -f $DIR/${s}/docker/docker-compose.yml"
     fi
 done
 
