65b01dcdadefdb0e08b90a6d08a26dd09fbbe7a6
Derk Snijders - i3anaan <i3anaan@gmail.com>
2017-12-17 03:45:37 +0100

    Updated deployment script

--- deploy.sh
+++ deploy.sh
@@ -4,21 +4,22 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 echo -e "###\n### Deploying OMS...\n###"
-if [ $(grep ADMIN_PASSWORD $DIR/oms-core.env) == "ADMIN_PASSWORD=1234" ]; then
+if [ $(grep ADMIN_PASSWORD $DIR/oms-core.env) == "ADMIN_PASSWORD=admin" ]; then
   echo "ERROR: Please do not use the default ADMIN_PASSWORD in ./oms-core.env"
   exit 9
 fi
 bash $DIR/oms-docker/oms.sh down -v
 rm -Rf oms-docker
+bash docker stop $(docker ps -aq)
+bash docker rmi $(docker images -a)
 echo -e "\nFinished cleanup\n"
 
-git clone --recursive --branch dev https://github.com/AEGEE/oms-docker.git
+git clone --recursive --branch master https://github.com/AEGEE/oms-docker.git
 cp $DIR/oms-core.env $DIR/oms-docker/oms-core/.env.example
 echo -e "\nFinished setting up files\n"
 
-bash $DIR/oms-docker/oms.sh up
+bash $DIR/oms-docker/oms.sh up -d
 echo -e "\nInitializing..."
 echo -e "This might take a while"
 sleep 5
-echo -e "Showing logs, feel free to cancel with ctrl+c (server keeps running)"
-bash $DIR/oms-docker/oms.sh logs -f
+echo -e "Use 'bash ./oms-docker/oms.sh logs -f' to show the log"
