5283211b64e6f8d758b5749193c77b1424ab298b
grasshopper <bellicaf@tcd.ie>
2018-01-23 14:54:15 +0100

    [wip] improving a bit of this and that

---
+++ password-setter.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+#check if we should NOT be strict (default: strict, to enforce secure policy)
+strict="true"
+if [ $# -eq 2 ]; then
+  if [ "$1" == "--loose" ]; then
+    $strict="false"
+  fi
+else
+  echo -e "Usage: password-setter.sh [--loose], assuming strict"
+fi
+#strict mode requires all passwords to be set and exits complaining
+#loose mode just exits (?)
+
+#Passwords set until now:
+OMSCORE_PW="superAdmin"
+OMSCORE_PW_LOCATION="oms-core/.env"
+#PW_POSTGRES=superAdmin #oms-core/docker/docker-compose.yml BUT you can put it in the above file and make the composer take it from there
+#PW_PGADMIN=superAdmin #set in the container probably
+#PW_MONGO=superAdmin #?
+#PW_MONGOADMIN=superAdmin #oms-global/docker/mongoui/app.json
+#PW_MEDIAWIKI=superAdmin
+#PW_LIMESURVEY=superAdmin
+#PW_MONITORING=superAdmin
+
+passwords=($OMSCORE_PW)
+locations=($OMSCORE_PW_LOCATION)
+
+COUNTER=0
+while [  $COUNTER -lt ${#passwords[@]} ]; do
+ currPw=${passwords[$COUNTER]}
+ currServ=${locations[$COUNTER]}
+ sed -i s/adminpass/$currPw/ "$currServ"
+ let COUNTER=COUNTER+1
+done
+
+
+
+#others:
+#bugsnagKey in configFile.json of oms-events/docker
+#oauth keys in .env of oms-core
