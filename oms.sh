d3780aca65c857c8d4c6397caca55ba7c7bab364
Nico Westerbeck <Nico.Westerbeck@mailbox.tu-dresden.de>
2018-04-18 13:44:11 +0200

    Integrated new core and loginservice

--- oms.sh
+++ oms.sh
@@ -16,6 +16,22 @@ docker network create OMS
 
 ## Export all environment variables from .env to this script in case we need them some time
 export $(cat .env | grep -v ^# | xargs)
+
+## Create secrets
+echo -e "[OMS] Creating random secrets if not existing"
+## Create a new random jwt key
+if ! [[ -f "$DIR/secrets/jwt_key" ]]; then
+  mkdir -p $DIR/secrets
+  cat /dev/random | head -c 256 | base64 > $DIR/secrets/jwt_key
+fi
+
+## Will not result in a valid sendgrid key but at least allows starting of the docker-compose stack
+if ! [[ -f "$DIR/secrets/sendgrid_key" ]]; then
+  mkdir -p $DIR/secrets
+  cat /dev/random | head -c 256 | base64 > $DIR/secrets/sendgrid_key
+fi
+
+
 ## ENABLED_SERVICES holds a string separated by : with all enabled services (like "oms-global:omscore:oms-serviceregistry")
 ## If you want to change the enabled services, change the array in .env
 service_string=$(printenv ENABLED_SERVICES)
