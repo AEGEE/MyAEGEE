bef47976a39d3be6d476b4a587c688711134b552
Sergey Peshkov <serge1peshcoff@mail.ru>
2018-06-18 00:23:33 +0300

    Enabled async syntax for Webpack

--- frontend/build/webpack.base.conf.js
+++ frontend/build/webpack.base.conf.js
@@ -7,7 +7,7 @@ const projectRoot = path.resolve(__dirname, '../')
 
 module.exports = {
   entry: {
-    app: ['./client/index.js'],
+    app: ['babel-polyfill', './client/index.js'],
     // If you want to support IE < 11, should add `babel-polyfill` to vendor.
     // e.g. ['babel-polyfill', 'vue', 'vue-router', 'vuex']
     vendor: [
