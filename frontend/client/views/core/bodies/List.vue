2d6acb96878ec2d833056aa603456c7b91094d96
Sergey Peshkov <serge1peshcoff@mail.ru>
2018-06-20 12:01:51 +0300

    Bodies listing

---
+++ frontend/client/views/core/bodies/List.vue
@@ -0,0 +1,122 @@
+<template>
+  <div class="tile is-ancestor">
+    <div class="tile is-parent is-vertical">
+      <article class="tile is-child">
+        <h4 class="title">Bodies list</h4>
+        <div class="field">
+          <label class="label">Search by name or description</label>
+          <div class="control">
+            <input class="input" type="text" v-model="query" placeholder="Search by name or description" @input="refetch()">
+          </div>
+        </div>
+
+        <div class="table-responsive">
+          <table class="table is-bordered is-striped is-narrow is-fullwidth">
+            <thead>
+              <tr>
+                <th>Body ID</th>
+                <th>Name and surname</th>
+                <th>Description</th>
+                <th>Link</th>
+              </tr>
+            </thead>
+            <tfoot>
+              <tr>
+                <th>Body ID</th>
+                <th>Name and surname</th>
+                <th>Description</th>
+                <th>Link</th>
+              </tr>
+            </tfoot>
+            <tbody>
+              <tr v-show="bodies.length" v-for="body in bodies" v-bind:key="body.id">
+                <td>{{ body.id }}</td>
+                <td>{{ body.name }}</td>
+                <td>{{ body.description }}</td>
+                <td>
+                  <router-link :to="{ name: 'oms.bodies.view', params: { id: body.id } }">
+                    {{ '/bodies/' + body.id }}
+                  </router-link>
+                </td>
+              </tr>
+              <tr v-show="!bodies.length && !isLoading">
+                <td colspan="4" class="has-text-centered">Bodies list is empty</td>
+              </tr>
+              <tr v-show="isLoading">
+                <td colspan="4" class="has-text-centered"><i style="font-size:24px" class="fa fa-spinner fa-spin"></i></td>
+              </tr>
+            </tbody>
+          </table>
+
+          <div class="field">
+            <button
+              class="button is-primary is-fullwidth"
+              :class="{ 'is-loading': isLoading }"
+              :disabled="isLoading"
+              v-show="canLoadMore"
+              @click="fetchData()">Load more bodies</button>
+          </div>
+        </div>
+      </article>
+    </div>
+  </div>
+</template>
+
+<script>
+import services from '../../../services.json'
+
+export default {
+  name: 'BodiesList',
+  data () {
+    return {
+      bodies: [],
+      isLoading: false,
+      query: '',
+      limit: 30,
+      offset: 0,
+      canLoadMore: true,
+      source: null
+    }
+  },
+  computed: {
+    queryObject () {
+      const queryObj = {
+        limit: this.limit,
+        offset: this.offset
+      }
+
+      if (this.query) queryObj.query = this.query
+      return queryObj
+    }
+  },
+  methods: {
+    refetch () {
+      this.bodies = []
+      this.offset = 0
+      this.canLoadMore = true
+      this.fetchData()
+    },
+    fetchData (state) {
+      this.isLoading = true
+      if (this.source) this.source.cancel()
+      this.source = this.axios.CancelToken.source()
+
+      this.axios.get(services['oms-core-elixir'] + '/bodies', { params: this.queryObject, cancelToken: this.source.token }).then((response) => {
+        this.bodies = this.bodies.concat(response.data.data)
+        this.offset += this.limit
+        this.canLoadMore = response.data.data.length === this.limit
+        this.isLoading = false
+      }).catch((err) => {
+        if (this.axios.isCancel(err)) {
+          return console.debug('Request cancelled.')
+        }
+
+        throw err
+      })
+    }
+  },
+  mounted () {
+    this.fetchData()
+  }
+}
+</script>
---
+++ frontend/client/views/core/members/Edit.vue
@@ -0,0 +1,183 @@
+<template>
+  <div class="tile is-ancestor ">
+    <div class="tile is-child">
+      <form @submit.prevent="saveUser()">
+        <div class="field">
+          <label class="label">First name</label>
+          <div class="control">
+            <input class="input" type="text" required v-model="user.first_name" placeholder="Type your first name..." />
+          </div>
+          <p class="help is-danger" v-if="errors.first_name">{{ errors.first_name.join(', ')}}</p>
+        </div>
+
+        <div class="field">
+          <label class="label">Last name</label>
+          <div class="control">
+            <input class="input" type="text" required v-model="user.last_name" placeholder="Type your last name..." />
+          </div>
+          <p class="help is-danger" v-if="errors.last_name">{{ errors.last_name.join(', ')}}</p>
+        </div>
+
+        <div class="field">
+          <label class="label">Email</label>
+          <div class="control has-icons-left">
+            <span class="icon is-small is-left"><i class="fa fa-envelope"></i></span>
+            <input class="input" type="text" v-model="user.user.email" />
+          </div>
+          <p class="help is-danger" v-if="errors.email">{{ errors.email.join(', ')}}</p>
+        </div>
+
+        <div class="field">
+          <label class="label">Phone</label>
+          <div class="control has-icons-left">
+            <span class="icon is-small is-left"><i class="fa fa-phone"></i></span>
+            <input class="input" type="text" v-model="user.phone" />
+          </div>
+          <p class="help is-danger" v-if="errors.phone">{{ errors.phone.join(', ')}}</p>
+        </div>
+
+        <div class="field">
+          <label class="label">Birthday</label>
+          <b-datepicker :date-formatter="formatDate" :date-parser="parseDate" v-model="birthday" @input="transformBirthday()" />
+          <p class="help is-danger" v-if="errors.birthday">{{ errors.birthday.join(', ')}}</p>
+        </div>
+
+        <div class="field">
+          <label class="label">Primary body</label>
+          <p class="help" v-if="user.primary_body">{{ user.primary_body.name }}</p>
+          <p class="help" v-if="!user.primary_body"><i>Not set.</i></p>
+          <b-autocomplete
+            v-model="autocompleteBody"
+            :data="user.bodies"
+            open-on-focus="true"
+            @select="body => { user.primary_body_id = body.id; user.primary_body = body } ">
+            <template slot-scope="props">
+              <div class="media">
+                <div class="media-content">
+                    {{ props.option.name }}
+                    <br>
+                    <small> {{ props.option.description }} </small>
+                </div>
+              </div>
+            </template>
+          </b-autocomplete>
+          <p class="help is-danger" v-if="errors.primary_body_id">{{ errors.primary_body_id.join(', ')}}</p>
+        </div>
+
+        <div class="field">
+          <label class="label">Gender</label>
+          <div class="control">
+            <input class="input" type="text" v-model="user.gender" />
+          </div>
+          <p class="help is-danger" v-if="errors.gender">{{ errors.gender.join(', ')}}</p>
+        </div>
+
+        <div class="field">
+          <label class="label">Address</label>
+          <div class="control">
+            <input class="input" type="text" v-model="user.address" />
+          </div>
+          <p class="help is-danger" v-if="errors.address">{{ errors.address.join(', ')}}</p>
+        </div>
+
+        <div class="field">
+          <label class="label">About me</label>
+          <div class="control">
+            <input class="input" type="text" v-model="user.about_me" />
+          </div>
+          <p class="help is-danger" v-if="errors.about_me">{{ errors.about_me.join(', ')}}</p>
+        </div>
+
+        <b-loading is-full-page="false" :active.sync="isLoading"></b-loading>
+
+        <div class="field">
+          <div class="control">
+            <input type="submit" value="Save user" :disabled="isSaving" class="button is-primary is-fullwidth"/>
+          </div>
+        </div>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+import moment from 'moment'
+import services from '../../../services.json'
+
+export default {
+  data () {
+    return {
+      user: {
+        name: '',
+        surname: '',
+        id: null,
+        seo_url: null,
+        primary_body_id: null,
+        primary_body: null,
+        circles: null,
+        date_of_birth: null,
+        gender: null,
+        phone: null,
+        user: {},
+        bodies: []
+      },
+      errors: {},
+      birthday: null,
+      isLoading: true,
+      isSaving: false
+    }
+  },
+  methods: {
+    parseDate (date) {
+      return moment(date, 'YYYY-MM-DD').toDate()
+    },
+    formatDate (date) {
+      return moment(date).format('YYYY-MM-DD')
+    },
+    transformBirthday () {
+      this.user.date_of_birth = moment(this.birthday, 'YYYY-MM-DD').toDate()
+    },
+    saveUser () {
+      this.isSaving = true
+      this.errors = {}
+
+      this.axios.put(services['oms-core-elixir'] + '/members/' + this.$route.params.id, { member: this.user }).then((response) => {
+        this.isSaving = false
+
+        this.$toast.open({
+          duration: 3000,
+          message: 'User is saved.',
+          type: 'is-success'
+        })
+
+        return this.$router.push({
+          name: 'oms.members.view',
+          params: { id: this.user.seo_url || this.user.id }
+        })
+      }).catch((err) => {
+        if (err.response.status === 422) { // validation errors
+          this.errors = err.response.data.errors
+          return this.$toast.open({
+            duration: 3000,
+            message: 'Some of the user data is invalid.',
+            type: 'is-danger'
+          })
+        }
+
+        this.$toast.open({
+          duration: 3000,
+          message: 'Could not save user: ' + err.message,
+          type: 'is-danger'
+        })
+      })
+    }
+  },
+  mounted () {
+    this.axios.get(services['oms-core-elixir'] + '/members/' + this.$route.params.id).then((response) => {
+      this.user = response.data.data
+      this.birthday = response.data.data.date_of_birth ? moment(response.data.data.date_of_birth, 'YYYY-MM-DD').toDate() : null
+      this.isLoading = false
+    })
+  }
+}
+</script>
