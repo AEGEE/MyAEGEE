ad8c631cb18234261eefab9244db3cd1e993c3ae
Sergey Peshkov <serge1peshcoff@mail.ru>
2018-06-21 20:32:57 +0300

    Footer message

--- frontend/client/components/layout/FooterBar.vue
+++ frontend/client/components/layout/FooterBar.vue
@@ -2,21 +2,18 @@
   <footer class="footer">
     <div class="container">
       <div class="content has-text-centered">
-        <p class="social">
-          <a :href="'https://github.com/' + repository">
-            <span class="icon">
-              <i class="fa fa-github"></i>
-            </span>
-          </a>
-          <a href="https://twitter.com/_fundon">
-            <span class="icon">
-              <i class="fa fa-twitter"></i>
-            </span>
-          </a>
-        </p>
-        <p><span class="icon"><i class="fa fa-code"></i></span> with <span class="icon"><i class="fa fa-heart"></i></span> by <a href="https://github.com/fundon">fundon</a>.</p>
-        <p>Code licensed under <a :href="'https://github.com/' + repository + '/blob/master/LICENSE'">{{ license }}</a>.</p>
+        <p>© 2017 AEGEE - OMS-Frontend version {{ version }}</p>
+        <p>Powered by <a href="//oms.aegee.org">Online membership system</a></p>
       </div>
+
+      <article class="message is-danger">
+        <div class="message-header">
+          <p>DEBUG BUILD</p>
+        </div>
+        <div class="message-body">
+           <strong>All data is volatile and can be reverted at any time!</strong>
+        </div>
+      </article>
     </div>
   </footer>
 </template>
