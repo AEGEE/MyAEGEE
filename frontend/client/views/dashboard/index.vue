bf2898795e6b509fab7530156a1b715d0f4cee6c
Sergey Peshkov <serge1peshcoff@mail.ru>
2018-06-22 00:09:42 +0300

    Removed complicated stuff from dashboard

--- frontend/client/views/dashboard/index.vue
+++ frontend/client/views/dashboard/index.vue
@@ -28,25 +28,6 @@
     </div>
 
     <div class="tile is-ancestor">
-      <div class="tile is-parent is-6">
-        <article class="tile is-child box">
-          <h4 class="title">Five</h4>
-          <div class="content">
-            <chart :type="'doughnut'" :data="chartData"></chart>
-          </div>
-        </article>
-      </div>
-      <div class="tile is-parent is-6">
-        <article class="tile is-child box">
-          <h4 class="title">Six</h4>
-          <div class="content">
-            <chart :type="'pie'" :data="chartData"></chart>
-          </div>
-        </article>
-      </div>
-    </div>
-
-    <div class="tile is-ancestor">
       <div class="tile is-vertical is-9">
         <div class="tile">
           <div class="tile is-parent">
@@ -106,47 +87,14 @@
 </template>
 
 <script>
-import Chart from 'vue-bulma-chartjs'
-
 export default {
-  components: {
-    Chart
-  },
-
   data () {
     return {
-      data: [300, 50, 100]
+
     }
   },
-
-  computed: {
-    chartData () {
-      return {
-        labels: [
-          'Red',
-          'Blue',
-          'Yellow'
-        ],
-        datasets: [{
-          data: this.data,
-          backgroundColor: [
-            '#FF6384',
-            '#36A2EB',
-            '#FFCE56'
-          ]
-        }]
-      }
-    }
-  },
-
   mounted () {
-    setInterval(() => {
-      // https://github.com/vuejs/vue/issues/2873
-      // Array.prototype.$set/$remove deprecated (use Vue.set or Array.prototype.splice instead)
-      this.data.forEach((item, i) => {
-        this.data.splice(i, 1, Math.ceil(Math.random() * 1000))
-      })
-    }, 1024)
+
   }
 }
 </script>
