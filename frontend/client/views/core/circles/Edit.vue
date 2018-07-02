e19f8196bc287a94154941a36200c51ed984a7eb
Sergey Peshkov <serge1peshcoff@mail.ru>
2018-07-03 01:11:09 +0300

    Editing parent and child circles and permissions for single circle

--- frontend/client/views/core/circles/Edit.vue
+++ frontend/client/views/core/circles/Edit.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="tile is-ancestor ">
+  <div class="tile is-ancestor is-vertical">
     <div class="tile is-child">
       <form @submit.prevent="saveCircle()">
         <div class="field">
@@ -34,6 +34,119 @@
         </div>
       </form>
     </div>
+
+    <hr/>
+
+    <div class="tile is-child" v-if="$route.params.id">
+      <div class="field">
+        <label class="label">Parent circle</label>
+        <div class="control">
+          <div class="field has-addons">
+            <b-autocomplete
+              v-model="autoComplete.parentCircle.name"
+              :data="autoComplete.parentCircle.values"
+              open-on-focus="true"
+              :loading="autoComplete.parentCircle.loading"
+              @input="query => fetchSomething(query, 'parentCircle', 'circles')"
+              @select="childCircle => setParentCircle(childCircle)">
+              <template slot-scope="props">
+                <div class="media">
+                  <div class="media-content">
+                      {{ props.option.name }}
+                      <br>
+                      <small> {{ props.option.description }} </small>
+                  </div>
+                </div>
+              </template>
+            </b-autocomplete>
+            <p class="control">
+              <a class="button is-danger"
+                @click="setParentCircle(null)"
+                v-if="circle.parent_circle">{{ circle.parent_circle.name }} (Click to unset)</a>
+              <a class="button is-static" v-if="!circle.parent_circle">Not set.</a>
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <div class="field">
+        <label class="label">Child circles</label>
+        <ul>
+          <li v-for="childCircle in circle.child_circles" v-bind:key="childCircle.id">
+            <span class="tag is-danger">{{ childCircle.name }}
+              <button class="delete is-small" @click="toggleChildCircle(childCircle, false)"></button>
+            </span>
+          </li>
+          <li v-if="circle.child_circles.length === 0">
+            <i>No child circles attached.</i>
+          </li>
+        </ul>
+      </div>
+
+      <div class="field">
+        <label class="label">Add child circle</label>
+        <div class="control">
+          <div class="field has-addons">
+            <b-autocomplete
+              v-model="autoComplete.childCircles.name"
+              :data="autoComplete.childCircles.values"
+              open-on-focus="true"
+              :loading="autoComplete.childCircles.loading"
+              @input="query => fetchSomething(query, 'childCircles', 'circles')"
+              @select="childCircle => toggleChildCircle(childCircle, true)">
+              <template slot-scope="props">
+                <div class="media">
+                  <div class="media-content">
+                      {{ props.option.name }}
+                      <br>
+                      <small> {{ props.option.description }} </small>
+                  </div>
+                </div>
+              </template>
+            </b-autocomplete>
+          </div>
+        </div>
+      </div>
+
+      <div class="field">
+        <label class="label">Attached permissions</label>
+        <ul>
+          <li v-for="permission in circle.permissions" v-bind:key="permission.id">
+            <span class="tag is-danger">{{ permission.combined }}
+              <button class="delete is-small" @click="togglePermission(permission, false)"></button>
+            </span>
+          </li>
+          <li v-if="circle.permissions.length === 0">
+            <i>No permissions attached.</i>
+          </li>
+        </ul>
+      </div>
+
+      <div class="field">
+        <label class="label">Add permission</label>
+        <div class="control">
+          <div class="field has-addons">
+            <b-autocomplete
+              v-model="autoComplete.permissions.name"
+              :data="autoComplete.permissions.values"
+              open-on-focus="true"
+              :loading="autoComplete.permissions.loading"
+              @input="query => fetchSomething(query, 'permissions', 'permissions')"
+              @select="permission => togglePermission(permission, true)">
+              <template slot-scope="props">
+                <div class="media">
+                  <div class="media-content">
+                      {{ props.option.combined }}
+                      <br>
+                      <small> {{ props.option.description }} </small>
+                  </div>
+                </div>
+              </template>
+            </b-autocomplete>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -55,6 +168,12 @@ export default {
         parent_circle: null,
         parent_circle_id: null
       },
+      autoComplete: {
+        parentCircle: { name: '', values: [], loading: false },
+        childCircles: { name: '', values: [], loading: false },
+        permissions: { name: '', values: [], loading: false }
+      },
+      token: null,
       errors: {},
       isLoading: false,
       isSaving: false
@@ -62,6 +181,155 @@ export default {
   },
   computed: mapGetters(['services']),
   methods: {
+    fetchSomething (query, key, context) {
+      if (!query) return
+
+      this.autoComplete[key].values = []
+      this.autoComplete[key].loading = true
+
+      if (this.token) this.token.cancel()
+      this.token = this.axios.CancelToken.source()
+
+      this.axios.get(this.services['oms-core-elixir'] + '/' + context, {
+        cancelToken: this.token.token,
+        params: { query }
+      }).then((response) => {
+        this.autoComplete[key].values = response.data.data
+        this.autoComplete[key].loading = false
+      }).catch((err) => {
+        if (this.axios.isCancel(err)) {
+          return console.log('Request canceled')
+        }
+
+        this.autoComplete[key].loading = false
+
+        this.$toast.open({
+          duration: 3000,
+          message: 'Could not fetch ' + context + 's: ' + err.message,
+          type: 'is-danger'
+        })
+      })
+    },
+    setParentCircle (circle) {
+      this.autoComplete.circles.loading = true
+      this.axios.put(this.services['oms-core-elixir'] + '/circles/' + this.$route.params.id + '/parent', {
+        parent_circle_id: circle ? circle.id : null
+      }).then((response) => {
+        this.autoComplete.circles.loading = false
+        this.circle.parent_circle = circle
+        this.circle.parent_circle_id = circle ? circle.id : null
+
+        this.$toast.open({
+          duration: 3000,
+          message: 'Parent circle is ' + (circle ? 'set.' : 'unset.'),
+          type: 'is-success'
+        })
+      }).catch((err) => {
+        this.autoComplete.circles.loading = false
+
+        if (err.response.status === 422) {
+          const errors = Object.keys(err.response.data.errors).map(key => err.response.data.errors[key].join(',')).join(',')
+          return this.$toast.open({
+            duration: 3000,
+            message: 'Could not ' + (circle ? 'set' : 'unset') + ' parent circle: ' + errors,
+            type: 'is-danger'
+          })
+        }
+
+        this.$toast.open({
+          duration: 3000,
+          message: 'Could not ' + (circle ? 'set' : 'unset') + ' parent circle: ' + err.message,
+          type: 'is-danger'
+        })
+      })
+    },
+    togglePermission (permission, add) {
+      this.autoComplete.permissions.loading = true
+
+      const permissionsBefore = JSON.parse(JSON.stringify(this.circle.permissions))
+
+      if (add) {
+        this.circle.permissions.push(permission)
+      } else {
+        const index = this.circle.permissions.findIndex(perm => perm.id === permission.id)
+        this.circle.permissions.splice(index, 1)
+      }
+
+      this.axios.put(this.services['oms-core-elixir'] + '/circles/' + this.$route.params.id + '/permissions', {
+        permissions: this.circle.permissions
+      }).then((response) => {
+        this.autoComplete.permissions.loading = false
+
+        this.$toast.open({
+          duration: 3000,
+          message: 'Permission is ' + (add ? 'added. ' : 'removed.'),
+          type: 'is-success'
+        })
+      }).catch((err) => {
+        this.autoComplete.permissions.loading = false
+        this.circle.permissions = permissionsBefore // reverting permissions list
+
+        if (err.response.status === 422) {
+          const errors = Object.keys(err.response.data.errors).map(key => err.response.data.errors[key].join(',')).join(',')
+          return this.$toast.open({
+            duration: 3000,
+            message: 'Could not ' + (add ? 'add' : 'remove') + ' permission: ' + errors,
+            type: 'is-danger'
+          })
+        }
+
+        this.$toast.open({
+          duration: 3000,
+          message: 'Could not ' + (add ? 'add' : 'remove') + ' permission: ' + err.message,
+          type: 'is-danger'
+        })
+      })
+    },
+    toggleChildCircle (circle, add) {
+      this.autoComplete.childCircles.loading = true
+
+      const circlesBefore = JSON.parse(JSON.stringify(this.circle.child_circles))
+
+      if (add) {
+        this.circle.child_circles.push(circle)
+      } else {
+        const index = this.circle.child_circles.findIndex(newCircle => newCircle.id === circle.id)
+        this.circle.child_circles.splice(index, 1)
+      }
+
+      this.axios.put(this.services['oms-core-elixir'] + '/circles/' + this.$route.params.id + '/children', {
+        child_circles: this.circle.child_circles
+      }).then((response) => {
+        this.autoComplete.childCircles.loading = false
+
+        this.$toast.open({
+          duration: 3000,
+          message: 'Child circle is ' + (add ? 'added. ' : 'removed.'),
+          type: 'is-success'
+        })
+      }).catch((err) => {
+        this.autoComplete.childCircles.loading = false
+        this.circle.child_circles = circlesBefore // reverting children list
+
+        if (err.response.status === 422) {
+          const errors =
+            err.response.data.errors
+              ? Object.keys(err.response.data.errors).map(key => err.response.data.errors[key].join(',')).join(',')
+              : err.response.data.message
+          return this.$toast.open({
+            duration: 3000,
+            message: 'Could not ' + (add ? 'add' : 'remove') + ' child circle: ' + errors,
+            type: 'is-danger'
+          })
+        }
+
+        this.$toast.open({
+          duration: 3000,
+          message: 'Could not ' + (add ? 'add' : 'remove') + ' child circle: ' + err.message,
+          type: 'is-danger'
+        })
+      })
+    },
     saveCircle () {
       this.isSaving = true
       this.errors = {}
@@ -101,6 +369,11 @@ export default {
           type: 'is-danger'
         })
       })
+    },
+    capitalize (value) {
+      if (!value) return ''
+      value = value.toString()
+      return value.charAt(0).toUpperCase() + value.slice(1)
     }
   },
   mounted () {
