ce6e7a7902798c24b3da03cdd1f796ca8c4e9791
Sergey Peshkov <serge1peshcoff@mail.ru>
2018-07-11 00:18:26 +0300

    Apply and approve pax page

---
+++ frontend/client/views/events/Apply.vue
@@ -0,0 +1,258 @@
+<template>
+  <div class="tile is-ancestor">
+    <div class="tile is-parent">
+      <div class="tile is-child">
+        <div class="title" v-show="isNew">Apply to {{ event.name }}</div>
+        <div class="title" v-show="!isNew && application.status === 'requesting'">Edit your application on {{ event.name }}</div>
+        <div class="title" v-show="!isNew && application.status !== 'requesting'">See your application on {{ event.name }}</div>
+
+        <form @submit.prevent="saveApplication()">
+          <div class="tile is-parent" v-show="isNew || (application.status === 'requesting')">
+            <div class="tile is-child">
+              <div class="field">
+                <label class="label">Select body to apply from</label>
+                <div class="control">
+                  <div class="field has-addons">
+                    <b-autocomplete
+                      v-model="autoComplete.bodies.name"
+                      :data="loginUser.bodies"
+                      open-on-focus="true"
+                      @select="body => { application.body_id = body.id; application.body = body; saved = false }">
+                      <template slot-scope="props">
+                        <div class="media">
+                          <div class="media-content">
+                            {{ props.option.name }}
+                            <br>
+                            <small> {{ props.option.description }} </small>
+                          </div>
+                        </div>
+                      </template>
+                    </b-autocomplete>
+                    <p class="control">
+                      <a class="button is-danger"
+                        @click="body => { application.body_id = null; application.body = null; saved = false }"
+                        v-if="application.body">{{ application.body.name }} (Click to unset)</a>
+                      <a class="button is-static" v-if="!application.body">Not set.</a>
+                    </p>
+                  </div>
+                </div>
+              </div>
+
+              <div class="field is-fullwidth" v-for="field in event.application_fields" v-bind:key="field._id">
+                <div class="control">
+                  <label class="has-text-weight-bold">{{ field.name }}<span class="has-text-danger" v-show="!field.optional">*</span></label>
+                  <small>{{ field.description }}</small>
+                </div>
+                <div class="control">
+                  <input
+                    type="text"
+                    class="input"
+                    :required="!field.optional"
+                    v-model="field.answer"
+                    @input="saved=false"
+                    />
+                </div>
+              </div>
+
+              <div class="field" v-show="event.application_fields.length == 0">
+                <p class="notification is-info">
+                  You do not need to fill in any application fields to apply for this event! Just press the button.
+                </p>
+              </div>
+
+              <div class="field">
+                <button type="submit" class="button is-info" v-show="!saved">
+                  Save application!
+                </button>
+                <button type="submit" class="button is-primary" disabled="disabled" v-show="saved">
+                  Application saved!
+                </button>
+              </div>
+            </div>
+          </div>
+
+          <div v-show="!isNew && application.status !== 'requesting'" class="tile is-parent">
+            <div class="tile is-child">
+              <p>
+                <b>You applied from this body:</b>
+                <router-link :to="{ name: 'oms.bodies.view', params: { id: application.body_id } }">
+                  {{ application.body ? application.body.name : 'Loading...' }}
+                </router-link>
+              </p>
+              <table class="table is-narrow is-fullwidth">
+                <thead>
+                  <tr>
+                    <th>Question</th>
+                    <th>Description</th>
+                    <th>Your answer</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr v-for="field in event.application_fields" v-bind:key="field._id">
+                    <td><b>{{ field.name }}</b><span class="is-danger" ng-show="!field.optional">*</span></td>
+                    <td>{{ field.description }}</td>
+                    <td>{{ field.answer }}</td>
+                  </tr>
+                </tbody>
+              </table>
+
+              <div class="field" v-show="event.application_fields.length == 0">
+                <div class="notification is-info">
+                  You didn't need to fill in the application fields.
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div class="tile is-parent" v-show="!isNew">
+            <div class="tile is-child">
+              <div class="notification is-info" v-if="application.status === 'requesting'">
+                Your application was saved, please wait for the organizers to close the application process and start evaluating.
+              </div>
+              <div class="notification is-warning" v-else-if="application.status === 'pending'">
+                Your application is being processed, please wait for the organizers to evaluate your application. Unfortunately you can not edit it any more.
+              </div>
+              <div class="notification is-success" v-else-if="application.status === 'accepted'">
+                Congratulations, you have been accepted to the event!
+              </div>
+              <div class="notification is-success" v-else-if="application.status === 'rejected'">
+                Sorry, but you were not accepted to the event.
+              </div>
+            </div>
+          </div>
+
+          <b-loading is-full-page="false" :active.sync="isLoading"></b-loading>
+        </form>
+      </div>
+    </div>
+  </div>
+
+</template>
+
+
+<script>
+import { mapGetters } from 'vuex'
+
+export default {
+  name: 'ApplyToEvent',
+  data () {
+    return {
+      event: {
+        name: null,
+        application_fields: []
+      },
+      autoComplete: { bodies: { name: '' } },
+      application: {
+        body: null,
+        body_id: null,
+        user: null,
+        user_id: null,
+        _id: null,
+        application: []
+      },
+      saved: true,
+      errors: {},
+      isLoading: false,
+      isSaving: false
+    }
+  },
+  methods: {
+    saveApplication () {
+      // Copy data from the form into an object to submit it in the format the backend needs it
+      this.isSaving = true
+
+      const toServer = {
+        body_id: this.application.body.id,
+        application: []
+      }
+
+      for (const field of this.event.application_fields) {
+        if (field.answer) {
+          toServer.application.push({
+            field_id: field._id,
+            value: field.answer
+          })
+        }
+      }
+
+      this.axios.put(this.services['oms-events'] + '/single/' + this.$route.params.id + '/participants/mine', toServer).then(() => {
+        this.$toast.open({
+          type: 'is-success',
+          message: 'Your application was saved, you can still edit it until the application period ends'
+        })
+
+        return this.$router.push({
+          name: 'oms.events.view',
+          params: { id: this.event.url || this.event.id }
+        })
+      }).catch((err) => {
+        this.isSaving = false
+
+        if (err.response.status === 422) { // validation errors
+          this.errors = err.response.data.errors
+          return this.$toast.open({
+            duration: 3000,
+            message: 'Some of the application data is invalid.',
+            type: 'is-danger'
+          })
+        }
+
+        this.$toast.open({
+          duration: 3000,
+          message: 'Could not save application: ' + err.message,
+          type: 'is-danger'
+        })
+      })
+    }
+  },
+  computed: {
+    ...mapGetters({
+      services: 'services',
+      loginUser: 'user'
+    }),
+    isNew () {
+      return !this.application._id
+    }
+  },
+  mounted () {
+    this.isLoading = true
+    this.axios.get(this.services['oms-events'] + '/single/' + this.$route.params.id).then((response) => {
+      this.event = response.data.data
+
+      return this.axios.get(this.services['oms-events'] + '/single/' + this.$route.params.id + '/participants/mine').then((application) => {
+        this.application = application.data.data
+        this.application.body = this.loginUser.bodies.find(body => body.id === this.application.body_id)
+
+        // Loop through application fields and assign them to our model
+        for (const field of this.application.application) {
+          // Find the matching application_field to our users application field
+          this.event.application_fields.some((item, index) => {
+            if (field.field_id === item._id) {
+              this.event.application_fields[index].answer = field.value
+              return true
+            }
+            return false
+          })
+        }
+
+        this.isLoading = false
+      }).catch((err) => {
+        this.isLoading = false
+        // if there's no application, just ignore, otherwise re-throw the error
+        if (err.response.status !== 404) {
+          throw err
+        }
+      })
+    }).catch((err) => {
+      let message = (err.response.status === 404) ? 'Event is not found' : 'Some error happened: ' + err.message
+
+      this.$toast.open({
+        duration: 3000,
+        message,
+        type: 'is-danger'
+      })
+      this.$router.push({ name: 'oms.events.list' })
+    })
+  }
+}
+</script>
---
+++ frontend/client/views/events/Participants.vue
@@ -0,0 +1,209 @@
+<template>
+  <div class="tile is-ancestor">
+    <div class="tile is-parent">
+      <div class="tile is-child">
+        <div class="title">Applications</div>
+
+        <table class="table is-narrow is-fullwidth">
+          <thead>
+            <tr>
+              <th>User ID</th>
+              <th>Name</th>
+              <th>Body</th>
+              <th v-if="can.approve_participants">Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="pax in applications" v-bind:key="pax.user_id">
+              <td>{{ pax.user_id }}</td>
+              <td>
+                <router-link :to="{ name: 'oms.members.view', params: { id: pax.user_id } }">
+                  {{ pax.user ? pax.user.first_name + ' ' + pax.user.last_name: 'Loading...' }}
+                </router-link>
+              </td>
+              <td>
+                <router-link :to="{ name: 'oms.bodies.view', params: { id: pax.body_id } }">
+                  {{ pax.body ? pax.body.name : 'Loading...' }}
+                </router-link>
+              </td>
+              <td v-if="can.approve_participants">
+                <span>{{ pax.status | capitalize }}</span>
+                <button class="button is-small is-primary" @click="showModal(pax)">
+                  <i class="fa fa-pencil"></i>
+                </button>
+              </td>
+            </tr>
+            <tr v-if="applications.length == 0 && !isLoading">
+              <td colspan="4">No applications yet!</td>
+            </tr>
+            <tr v-if="isLoading">
+              <td colspan="4">Loading...</td>
+            </tr>
+          </tbody>
+        </table>
+
+        <div class="tile is-parent" v-if="selected">
+          <div class="tile is-child">
+            <div class="title">Selected participant</div>
+            <table class="table is-narrow is-fullwidth">
+              <tr>
+                <td><b>First Name</b></td>
+                <td>{{ selected.user.first_name }}</td>
+              </tr>
+
+              <tr>
+                <td><b>Last Name</b></td>
+                <td>{{ selected.user.last_name }}</td>
+              </tr>
+
+              <tr>
+                <td><b>Body</b></td>
+                <td>{{ selected.body.name }}</td>
+              </tr>
+
+              <tr>
+                <td><b>Application date</b></td>
+                <td>{{ selected.createdAt | date }}</td>
+              </tr>
+
+              <tr v-for="field in event.application_fields" v-bind:key="field._id">
+                <td><b>{{ field.name }}</b></td>
+                <td>{{ field.answer }}</td>
+              </tr>
+
+              <tr>
+                <td><b>Board comment</b></td>
+                <td v-show="selected.board_comment">{{ selected.board_comment }}</td>
+                <td v-show="!selected.board_comment">-</td>
+              </tr>
+            </table>
+
+            <div class="field">
+              <button class="button is-danger" @click="changeState(selected, 'rejected')">
+                <span class="icon">
+                  <i class="fa fa-minus-circle"></i>
+                </span>
+                <span>Reject</span>
+              </button>
+              <button class="button is-primary" @click="changeState(selected, 'accepted')">
+                <span class="icon">
+                  <i class="fa fa-plus-circle"></i>
+                </span>
+                <span>Accept</span>
+              </button>
+              <button class="button" @click="changeState(selected, 'pending')">
+                <span class="icon">
+                  <i class="fa fa-circle-o"></i>
+                </span>
+                <span>Postpone</span>
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+
+export default {
+  name: 'ParticipantsList',
+  data () {
+    return {
+      event: {
+        name: null,
+        application_fields: []
+      },
+      can: {
+        approve_participants: false
+      },
+      applications: [],
+      isLoading: false,
+      isSaving: false,
+      selected: null
+    }
+  },
+  methods: {
+    showModal (application) {
+      // Loop through application fields and assign them to our model
+      application.application.forEach((field) => {
+        // Find the matching application_field to our users application field
+        this.event.application_fields.some((item, index) => {
+          if (field.field_id === item._id) {
+            this.event.application_fields[index].answer = field.value
+            return true
+          }
+          return false
+        })
+      })
+
+      this.selected = application
+    },
+    changeState (application, newState) {
+      // Store the change
+      this.axios.put(this.services['oms-events'] + '/single/' + this.$route.params.id + '/participants/' + application.id + '/status/', {
+        status: newState
+      }).then(() => {
+        application.status = newState
+        this.selected = null
+
+        this.$toast.open({
+          message: 'Participant status is updated.',
+          type: 'is-success'
+        })
+      }).catch((err) => {
+        let message = 'Could not update participant status: ' + err.message
+
+        this.$toast.open({
+          duration: 3000,
+          message,
+          type: 'is-danger'
+        })
+      })
+    }
+  },
+  computed: mapGetters({
+    services: 'services',
+    loginUser: 'user'
+  }),
+  filters: {
+    capitalize (value) {
+      return value.charAt(0).toUpperCase() + value.slice(1)
+    }
+  },
+  mounted () {
+    this.isLoading = true
+    this.axios.get(this.services['oms-events'] + '/single/' + this.$route.params.id).then((response) => {
+      this.event = response.data.data
+      this.can = response.data.permissions.can
+
+      return this.axios.get(this.services['oms-events'] + '/single/' + this.$route.params.id + '/participants')
+    }).then((application) => {
+      this.applications = application.data.data
+      this.isLoading = false
+
+      // Fetching users and bodies.
+      for (const application of this.applications) {
+        this.axios.get(this.services['oms-core-elixir'] + '/members/' + application.user_id).then((user) => {
+          const member = user.data.data
+          application.user = member
+          application.body = member.bodies.find(body => body.id === application.body_id)
+
+          this.$forceUpdate()
+        }).catch(console.error)
+      }
+    }).catch((err) => {
+      let message = (err.response.status === 404) ? 'Event is not found' : 'Some error happened: ' + err.message
+
+      this.$toast.open({
+        duration: 3000,
+        message,
+        type: 'is-danger'
+      })
+      this.$router.push({ name: 'oms.events.list' })
+    })
+  }
+}
+</script>
