bad229cd2995ac86326f878a4e79b8c2197737a1
Sergey Peshkov <serge1peshcoff@mail.ru>
2018-07-14 15:25:42 +0300

    Hide image upload dialog on creating event

--- frontend/client/views/events/Edit.vue
+++ frontend/client/views/events/Edit.vue
@@ -1,34 +1,36 @@
 <template>
   <div class="tile is-ancestor ">
     <div class="tile is-child">
-      <div class="subtitle">Update event image</div>
+      <div v-if="$route.params.id">
+        <div class="subtitle">Update event image</div>
 
-      <div class="field is-grouped">
-        <div class="control">
-          <div class="file has-name">
-            <label class="file-label">
-              <input class="file-input" type="file" name="resume" @change="setFile($event)">
-              <span class="file-cta">
-                <span class="file-icon">
-                  <i class="fa fa-upload"></i>
+        <div class="field is-grouped">
+          <div class="control">
+            <div class="file has-name">
+              <label class="file-label">
+                <input class="file-input" type="file" name="resume" @change="setFile($event)">
+                <span class="file-cta">
+                  <span class="file-icon">
+                    <i class="fa fa-upload"></i>
+                  </span>
+                  <span class="file-label">
+                    Choose a file
+                  </span>
                 </span>
-                <span class="file-label">
-                  Choose a file
+                <span class="file-name">
+                  {{ file ? file.name : 'Not set.' }}
                 </span>
-              </span>
-              <span class="file-name">
-                {{ file ? file.name : 'Not set.' }}
-              </span>
-            </label>
+              </label>
+            </div>
+          </div>
+
+          <div class="control">
+            <a class="button is-info" :disabled="!file" @click="updateImage()">Upload!</a>
           </div>
         </div>
 
-        <div class="control">
-          <a class="button is-info" :disabled="!file" @click="updateImage()">Upload!</a>
-        </div>
+        <hr />
       </div>
-
-      <hr />
 
       <form @submit.prevent="saveEvent()">
         <div class="field">
