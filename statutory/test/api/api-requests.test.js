acc6215ca47f84726c647484f3b60bb07529129f
Sergey Peshkov <serge1peshcoff@mail.ru>
2018-10-11 12:38:07 +0300

    More testing for core failure

--- statutory/test/api/api-requests.test.js
+++ statutory/test/api/api-requests.test.js
@@ -23,7 +23,7 @@ describe('API requests', () => {
         expect(res.body.success).toEqual(false);
     });
 
-    test('should fail if body oms-core returns net error', async () => {
+    test('should fail if body oms-core returns net error while fetching user', async () => {
         mock.mockAll({ core: { netError: true } });
 
         const res = await request({
@@ -38,7 +38,7 @@ describe('API requests', () => {
         expect(res.body.success).toEqual(false);
     });
 
-    test('should fail if body oms-core returns garbage', async () => {
+    test('should fail if oms-core returns garbage while fetching user', async () => {
         mock.mockAll({ core: { badResponse: true } });
 
         const res = await request({
@@ -53,8 +53,53 @@ describe('API requests', () => {
         expect(res.body.success).toEqual(false);
     });
 
-    test('should fail if body oms-core returns unsuccessful response', async () => {
+    test('should fail if body oms-core returns unsuccessful response while fetching user', async () => {
         mock.mockAll({ core: { unauthorized: true } });
+
+        const res = await request({
+            uri: '/',
+            method: 'GET',
+            headers: {
+                'X-Auth-Token': 'blablabla'
+            }
+        });
+
+        expect(res.statusCode).toEqual(403);
+        expect(res.body.success).toEqual(false);
+    });
+
+    test('should fail if body oms-core returns net error while fetching permissions', async () => {
+        mock.mockAll({ mainPermissions: { netError: true } });
+
+        const res = await request({
+            uri: '/',
+            method: 'POST',
+            headers: {
+                'X-Auth-Token': 'blablabla'
+            }
+        });
+
+        expect(res.statusCode).toEqual(500);
+        expect(res.body.success).toEqual(false);
+    });
+
+    test('should fail if oms-core returns garbage while fetching permissions', async () => {
+        mock.mockAll({ mainPermissions: { badResponse: true } });
+
+        const res = await request({
+            uri: '/',
+            method: 'GET',
+            headers: {
+                'X-Auth-Token': 'blablabla'
+            }
+        });
+
+        expect(res.statusCode).toEqual(500);
+        expect(res.body.success).toEqual(false);
+    });
+
+    test('should fail if body oms-core returns unsuccessful response while fetching permissions', async () => {
+        mock.mockAll({ mainPermissions: { unauthorized: true } });
 
         const res = await request({
             uri: '/',
@@ -82,6 +127,8 @@ describe('API requests', () => {
         expect(res.statusCode).toEqual(400);
         expect(res.body.success).toEqual(false);
     });
+
+
 
     test('should fail on accessing non-existant endpoint', async () => {
         const res = await request({
