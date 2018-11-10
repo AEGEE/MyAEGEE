8281b184803e6e963b739a3be044d9ed1d5ca8bd
Sergey Peshkov <serge1peshcoff@mail.ru>
2018-11-10 22:28:50 +0300

    CLI for statutory, like rails console

---
+++ statutory/cli.js
@@ -0,0 +1,15 @@
+const models = require('./models');
+const { Sequlize, sequelize, authenticate } = require('./lib/sequelize');
+
+global.authenticate = authenticate;
+global.Sequlize = Sequlize;
+global.sequelize = sequelize;
+
+for (const key in models) {
+    global[key] = models[key]; // importing models as global
+}
+
+authenticate().catch((err) => {
+    console.error('Error connecting to DB:', err.message);
+    process.quit(1);
+});
