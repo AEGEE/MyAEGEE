d0185484f92fa2a814e365c19d3db7d3dfaa96b7
grasshopper <bellicaf@tcd.ie>
2018-11-21 15:49:48 +0100

    (Impr) Now the script waits till oms-core is ready
    Also add a sort-of helper for windows hosts
    Remove useless script to make sure repo was with all submodules

--- vagrant-post-script/orchestrate_docker.sh
+++ vagrant-post-script/orchestrate_docker.sh
@@ -4,5 +4,11 @@
 cd /vagrant/
 #possible to use flag -d
 make bootstrap
-sleep 90 #to give time for the bootstrap
+#sleep 90 #to give time for the bootstrap
+while [[ true ]]; do
+  if [[ $(curl localhost:4000 -s | head | grep "<html>") == "<html>" ]]; then
+     break;
+  fi
+  sleep 2;
+done
 
