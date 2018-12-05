e5cac045833da2a605598e0ed82a84baa351357a
Sergey Peshkov <serge1peshcoff@mail.ru>
2018-12-05 13:02:27 +0300

    Using OMS mailer

--- statutory/lib/massmailer.js
+++ statutory/lib/massmailer.js
@@ -1,5 +1,4 @@
-const nodemailer = require('nodemailer');
-const { errors, communication } = require('oms-common-nodejs');
+const { errors } = require('oms-common-nodejs');
 const request = require('request-promise-native');
 
 const config = require('../config');
@@ -14,17 +13,11 @@ exports.sendAll = async (req, res) => {
         ? req.event.applications.filter(application => application.status === req.params.filter)
         : req.event.applications;
 
-    const stats = {
-        total: applications.length,
-        sent: 0,
-        errors: 0
-    };
-
-    const transporter = nodemailer.createTransport(config.mailer);
     logger.info(`Sending mass mailer to ${applications.length} users`);
-    logger.info(`Filter = ${req.params.filter}`);
+    logger.info(`Filter = ${req.params.filter || 'not set'}`);
 
-    const headers = await communication.getRequestHeaders(req);
+    const to = [];
+    const bodies = [];
 
     for (const application of applications) {
         // Fetching user
@@ -32,27 +25,62 @@ exports.sendAll = async (req, res) => {
             const membersBody = await request({
                 url: config.core.url + ':' + config.core.port + '/members/' + application.user_id,
                 method: 'GET',
-                headers,
+                headers: {
+                    'X-Requested-With': 'XMLHttpRequest',
+                    'X-Auth-Token': req.headers['x-auth-token'],
+                },
                 simple: false,
                 json: true,
             });
+            const body = membersBody.data.bodies.find(body => body.id === application.body_id);
+
+            // We can customize the letter a little bit by replacing { something } with corresponding field.
+            // TODO: Think, maybe use Pug or EJS for that?
             const email = membersBody.data.user.email;
+            const text = req.body.text
+                .replace(/\{ first_name \}/ig, membersBody.data.first_name)
+                .replace(/\{ last_name \}/ig, membersBody.data.last_name)
+                .replace(/\{ participant_type \}/ig, application.participant_type)
+                .replace(/\{ participant_order \}/ig, application.participant_order)
+                .replace(/\{ body_name \}/ig, body.name);
 
-            // Sending mail
-            const mailOptions = {
-                from: req.body.from, // sender address
-                to: email, // list of receivers
-                subject: req.body.subject, // Subject line
-                text: req.body.text // plain text body
-            };
+            // Using the custom oms-mailer template, it accepts only body as a parameter
+            // and sends the body as it was passed.
+            to.push(email);
+            bodies.push({ body: text });
 
-            logger.info('Sending mass mailer email to ' + email + '...');
-            await transporter.sendMail(mailOptions);
-            stats.sent++;
+            logger.info('Prepared email to ' + email + '...');
         } catch (err) {
-            stats.errors++;
-            logger.error('Delivering mail failed: ' + err.message);
+            logger.error('Preparing mail failed for ' + email + ':' + err.message);
         }
+    }
+
+    logger.info('Prepared letters: ' + bodies.length);
+
+    const mailerBody = await request({
+        url: config.mailer.url + ':' + config.mailer.port + '/',
+        method: 'POST',
+        headers: {
+            'X-Requested-With': 'XMLHttpRequest',
+            'X-Auth-Token': req.headers['x-auth-token'],
+        },
+        simple: false,
+        json: true,
+        body: {
+            from: req.body.from,
+            to,
+            subject: req.body.subject,
+            template: 'custom.html',
+            parameters: bodies
+        }
+    });
+
+    if (typeof mailerBody !== 'object') {
+        return errors.makeInternalError(res, 'Malformed response from mailer: ' + mailerBody);
+    }
+
+    if (!mailerBody.success) {
+        return errors.makeInternalError(res, 'Unsuccessful response from mailer: ' + mailerBody);
     }
 
     return res.json({
