e838dad94aba8f08d8895bc9793faa70b341c974
Sergey Peshkov <serge1peshcoff@mail.ru>
2018-12-10 02:49:37 +0300

    Logging and disabled for testing

--- statutory/lib/bugsnag.js
+++ statutory/lib/bugsnag.js
@@ -1,9 +1,13 @@
 const bugsnag = require('@bugsnag/js');
 
 const config = require('../config');
+const logger = require('./logger');
+
 
 const bugsnagClient = bugsnag({
-    apiKey: config.bugsnagKey
+    apiKey: config.bugsnagKey,
+    logger,
+    notifyReleaseStages: [ 'production' ]
 });
 
 module.exports = bugsnagClient;
