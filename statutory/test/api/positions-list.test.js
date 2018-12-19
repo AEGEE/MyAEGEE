fcf6f0ec12e7f0cb0989acfe335a11e85c13f2b8
Sergey Peshkov <serge1peshcoff@mail.ru>
2018-12-19 12:24:57 +0300

    Tests for listing candidates

--- statutory/test/api/positions-list.test.js
+++ statutory/test/api/positions-list.test.js
@@ -52,4 +52,119 @@ describe('Positions listing', () => {
         expect(res.body.data.length).toEqual(1);
         expect(res.body.data[0].id).toEqual(firstPosition.id);
     });
+
+    test('should list all of the approved candidates on /accepted', async () => {
+        const event = await generator.createEvent({ type: 'agora', applications: [] });
+        const position = await generator.createPosition({
+            candidates: [generator.generateCandidate({ status: 'approved' })]
+        }, event);
+
+        const res = await request({
+            uri: '/events/' + event.id + '/positions/approved',
+            method: 'GET',
+            headers: { 'X-Auth-Token': 'blablabla' }
+        });
+
+        expect(res.statusCode).toEqual(200);
+        expect(res.body.success).toEqual(true);
+        expect(res.body).not.toHaveProperty('errors');
+        expect(res.body).toHaveProperty('data');
+
+        expect(res.body.data.length).toEqual(1);
+        expect(res.body.data[0].id).toEqual(position.id);
+        expect(res.body.data[0].candidates.length).toEqual(1);
+        expect(res.body.data[0].candidates[0]).toHaveProperty('first_name');
+    });
+
+    test('should indicate that there is unapproved application on /accepted', async () => {
+        const event = await generator.createEvent({ type: 'agora', applications: [] });
+        const position = await generator.createPosition({
+            candidates: [generator.generateCandidate({ status: 'pending' })]
+        }, event);
+
+        const res = await request({
+            uri: '/events/' + event.id + '/positions/approved',
+            method: 'GET',
+            headers: { 'X-Auth-Token': 'blablabla' }
+        });
+
+        expect(res.statusCode).toEqual(200);
+        expect(res.body.success).toEqual(true);
+        expect(res.body).not.toHaveProperty('errors');
+        expect(res.body).toHaveProperty('data');
+
+        expect(res.body.data.length).toEqual(1);
+        expect(res.body.data[0].id).toEqual(position.id);
+        expect(res.body.data[0].candidates.length).toEqual(1);
+        expect(res.body.data[0].candidates[0]).not.toHaveProperty('first_name');
+    });
+
+
+    test('should not list rejected applications on /accepted', async () => {
+        const event = await generator.createEvent({ type: 'agora', applications: [] });
+        await generator.createPosition({
+            candidates: [generator.generateCandidate({ status: 'rejected' })]
+        }, event);
+
+        const res = await request({
+            uri: '/events/' + event.id + '/positions/approved',
+            method: 'GET',
+            headers: { 'X-Auth-Token': 'blablabla' }
+        });
+
+        expect(res.statusCode).toEqual(200);
+        expect(res.body.success).toEqual(true);
+        expect(res.body).not.toHaveProperty('errors');
+        expect(res.body).toHaveProperty('data');
+        expect(res.body.data.length).toEqual(1);
+        expect(res.body.data[0].candidates.length).toEqual(0);
+    });
+
+    test('should list all applications on /all', async () => {
+        const event = await generator.createEvent({ type: 'agora', applications: [] });
+        await generator.createPosition({
+            candidates: [
+                generator.generateCandidate({ status: 'rejected' }),
+                generator.generateCandidate({ status: 'approved' }),
+                generator.generateCandidate({ status: 'pending' })
+            ]
+        }, event);
+
+        const res = await request({
+            uri: '/events/' + event.id + '/positions/all',
+            method: 'GET',
+            headers: { 'X-Auth-Token': 'blablabla' }
+        });
+
+        expect(res.statusCode).toEqual(200);
+        expect(res.body.success).toEqual(true);
+        expect(res.body).not.toHaveProperty('errors');
+        expect(res.body).toHaveProperty('data');
+        expect(res.body.data.length).toEqual(1);
+        expect(res.body.data[0].candidates.length).toEqual(3);
+    });
+
+    test('should return 403 if you don\'t have permissions on /all', async () => {
+        mock.mockAll({ mainPermissions: { noPermissions: true } });
+
+        const event = await generator.createEvent({ type: 'agora', applications: [] });
+        await generator.createPosition({
+            candidates: [
+                generator.generateCandidate({ status: 'rejected' }),
+                generator.generateCandidate({ status: 'approved' }),
+                generator.generateCandidate({ status: 'pending' })
+            ]
+        }, event);
+
+        const res = await request({
+            uri: '/events/' + event.id + '/positions/all',
+            method: 'GET',
+            headers: { 'X-Auth-Token': 'blablabla' }
+        });
+
+        expect(res.statusCode).toEqual(403);
+        expect(res.body.success).toEqual(false);
+        expect(res.body).not.toHaveProperty('data');
+        expect(res.body).toHaveProperty('message');
+    });
 });
