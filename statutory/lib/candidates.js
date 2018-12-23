3fc94b1a1dd54cf5dd56f4c453ab3af5acaf26ee
Sergey Peshkov <serge1peshcoff@mail.ru>
2018-12-23 19:39:31 +0300

    Updating the position if enough candidated on status change

--- statutory/lib/candidates.js
+++ statutory/lib/candidates.js
@@ -98,6 +98,21 @@ exports.setCandidatureStatus = async (req, res) => {
 
     await req.candidate.update({ status: req.body.status });
 
+    // Checking if we have enough approve candidates and if the deadline has passed.
+    // If so, closing the deadline (can reopen manually later).
+    const candidatesCount = await Candidate.count({
+        where: {
+            position_id: req.position.id,
+            status: { [Sequelize.Op.ne]: 'rejected' }
+        }
+    });
+
+    if (moment().isAfter(req.position.ends)) {
+        await req.position.update({
+            status: candidatesCount > req.position.places ? 'closed' : 'open'
+        }, { hooks: false });
+    }
+
     return res.json({
         success: true,
         data: req.candidate
