b35918b3c537284096c44708e33f4efa803ad5c7
Sergey Peshkov <serge1peshcoff@mail.ru>
2018-12-25 13:06:32 +0300

    Hide board comment when displaying application

--- frontend/client/views/statutory/ViewApplication.vue
+++ frontend/client/views/statutory/ViewApplication.vue
@@ -76,11 +76,11 @@
                   <td v-if="application.participant_order">{{ application.participant_order }}</td>
                   <td v-if="!application.participant_order"><i>Not set yet.</i></td>
                 </tr>
-                <tr>
+                <!--<tr>
                   <th>Board comment</th>
                   <td v-if="application.board_comment">{{ application.board_comment }}</td>
                   <td v-if="!application.board_comment"><i>Not set yet.</i></td>
-                </tr>
+                </tr>-->
               </tbody>
             </table>
           </div>
