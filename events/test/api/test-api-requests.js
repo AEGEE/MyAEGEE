354c4efd78d9252b80d53521edbeba5e6f11a800
Sergey Peshkov <serge1peshcoff@mail.ru>
2019-01-12 15:48:33 +0300

    Fixed API requests tests

--- events/test/api/test-api-requests.js
+++ events/test/api/test-api-requests.js
@@ -1,6 +1,5 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
-const { communication } = require('oms-common-nodejs');
 
 const server = require('../../lib/server.js');
 const mock = require('../scripts/mock-core-registry');
@@ -9,13 +8,8 @@ const expect = chai.expect;
 chai.use(chaiHttp);
 
 describe('API requests', () => {
-  let omscoreStub;
-  let omsserviceregistryStub;
-
   beforeEach(async () => {
-    const mocked = mock.mockAll();
-    omscoreStub = mocked.omscoreStub;
-    omsserviceregistryStub = mocked.omsserviceregistryStub;
+    mock.mockAll();
   });
 
   it('should reject requests without X-Auth-Token', (done) => {
@@ -30,9 +24,7 @@ describe('API requests', () => {
   });
 
   it('should fail if the user is unauthorized', (done) => {
-    const mocked = mock.mockAll({ core: { unauthorized: true } });
-    omscoreStub = mocked.omscoreStub;
-    omsserviceregistryStub = mocked.omsserviceregistryStub;
+    mock.mockAll({ core: { unauthorized: true } });
 
     chai.request(server)
       .get('/getUser')
@@ -47,46 +39,8 @@ describe('API requests', () => {
       });
   });
 
-  it('should fail if the oms-serviceregistry is unaccessible', (done) => {
-    const mocked = mock.mockAll({ registry: { netError: true } });
-    omscoreStub = mocked.omscoreStub;
-    omsserviceregistryStub = mocked.omsserviceregistryStub;
-
-    chai.request(server)
-      .get('/getUser')
-      .set('X-Auth-Token', 'blablabla')
-      .end((err, res) => {
-        expect(res).to.have.status(500);
-        expect(res).to.be.json;
-        expect(res).to.be.a('object');
-        expect(res.body.success).to.be.false;
-
-        done();
-      });
-  });
-
   it('should fail if the oms-core is unaccessible', (done) => {
-    const mocked = mock.mockAll({ core: { netError: true } });
-    omscoreStub = mocked.omscoreStub;
-    omsserviceregistryStub = mocked.omsserviceregistryStub;
-
-    chai.request(server)
-      .get('/getUser')
-      .set('X-Auth-Token', 'blablabla')
-      .end((err, res) => {
-        expect(res).to.have.status(500);
-        expect(res).to.be.json;
-        expect(res).to.be.a('object');
-        expect(res.body.success).to.be.false;
-
-        done();
-      });
-  });
-
-  it('should fail if the oms-serviceregistry returns non-JSON data', (done) => {
-    const mocked = mock.mockAll({ registry: { badResponse: true } });
-    omscoreStub = mocked.omscoreStub;
-    omsserviceregistryStub = mocked.omsserviceregistryStub;
+    mock.mockAll({ core: { netError: true } });
 
     chai.request(server)
       .get('/getUser')
@@ -102,27 +56,7 @@ describe('API requests', () => {
   });
 
   it('should fail if the oms-core returns non-JSON data', (done) => {
-    const mocked = mock.mockAll({ core: { badResponse: true } });
-    omscoreStub = mocked.omscoreStub;
-    omsserviceregistryStub = mocked.omsserviceregistryStub;
-
-    chai.request(server)
-      .get('/getUser')
-      .set('X-Auth-Token', 'blablabla')
-      .end((err, res) => {
-        expect(res).to.have.status(500);
-        expect(res).to.be.json;
-        expect(res).to.be.a('object');
-        expect(res.body.success).to.be.false;
-
-        done();
-      });
-  });
-
-  it('should fail if the oms-serviceregistry returns unsuccessful response', (done) => {
-    const mocked = mock.mockAll({ registry: { unsuccessfulResponse: true } });
-    omscoreStub = mocked.omscoreStub;
-    omsserviceregistryStub = mocked.omsserviceregistryStub;
+    mock.mockAll({ core: { badResponse: true } });
 
     chai.request(server)
       .get('/getUser')
@@ -138,9 +72,7 @@ describe('API requests', () => {
   });
 
   it('should fail if the oms-core returns unsuccessful response', (done) => {
-    const mocked = mock.mockAll({ core: { unsuccessfulResponse: true } });
-    omscoreStub = mocked.omscoreStub;
-    omsserviceregistryStub = mocked.omsserviceregistryStub;
+    mock.mockAll({ core: { unsuccessfulResponse: true } });
 
     chai.request(server)
       .get('/getUser')
