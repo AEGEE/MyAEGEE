2a392fbbf5ceddca7e91e1b4693b2ff1d95f1744
Sergey Peshkov <serge1peshcoff@mail.ru>
2019-01-12 14:42:16 +0300

    Deleted event role model

--- events/lib/models/EventRole.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('../config/mongo');
-
-const eventRoleSchema = mongoose.Schema({
-  cfg_id: { type: String, required: true },
-  name: { type: String, required: true },
-  description: { type: String, required: true }
-});
-
-module.exports = mongoose.model('EventRole', eventRoleSchema);
