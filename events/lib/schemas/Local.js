71e52b58428543c6b3f92e9046b6cfa109ef1e52
Sergey Peshkov <serge1peshcoff@mail.ru>
2019-01-15 15:11:49 +0300

    Removed unused stuff

--- events/lib/schemas/Local.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-
-const Local = mongoose.Schema({
-  body_id: { type: Number, required: true },
-});
-
-module.exports = Local;
--- events/lib/schemas/Organizer.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const organizersSchema = mongoose.Schema({
-  comment: String,
-  user_id: { type: Number, required: true },
-  roles: { type: [String], required: false },
-}, { minimize: false });
-
-module.exports = organizersSchema;
