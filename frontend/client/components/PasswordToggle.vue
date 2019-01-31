d2436ac9b5860a866de7775cce849da9e8b8cbf3
Sergey Peshkov <serge1peshcoff@mail.ru>
2019-02-01 00:45:58 +0300

    PasswordToggle component

---
+++ frontend/client/components/PasswordToggle.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="field is-fullwidth">
+    <label class="label">{{ label }}</label>
+    <div class="control">
+
+      <div class="field has-addons">
+        <div class="control is-expanded">
+          <input class="input" :type="displayPassword ? 'text' : 'password'" minlength="8" placeholder="placeholder" :required="required" v-model="value">
+        </div>
+        <div class="control">
+          <a class="button is-primary" @click="displayPassword = !displayPassword">
+            <span class="icon is-small">
+              <i class="fas fa-eye"></i>
+            </span>
+          </a>
+        </div>
+      </div>
+    </div>
+    <slot name="errors-slot"></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PasswordToggle',
+  props: ['value', 'label', 'placeholder', 'required'],
+  data () {
+    return {
+      displayPassword: false
+    }
+  },
+  watch: {
+    value () {
+      this.$emit('input', this.value)
+    }
+  },
+  mounted () {
+
+  }
+}
+</script>
