45e784b491c88d199139b4abe3957783d1166e42
Sergey Peshkov <serge1peshcoff@mail.ru>
2019-02-03 15:02:30 +0300

    Pass questions to statutory test

--- mailer/test/omsmailer_web/controllers/page_controller_test.exs
+++ mailer/test/omsmailer_web/controllers/page_controller_test.exs
@@ -133,14 +133,14 @@ defmodule OmsmailerWeb.PageControllerTest do
 
   # Statutory applied
   test "POST / statutory applied", %{conn: conn} do
-    conn = post conn, "/", %{template: "statutory_applied.html", parameters: %{event: %{name: "test" }, application: %{}}, to: "test@aegee.org", subject: "pirates"}
+    conn = post conn, "/", %{template: "statutory_applied.html", parameters: %{event: %{name: "test", "questions": []}, application: %{}}, to: "test@aegee.org", subject: "pirates"}
     assert json_response(conn, 200)
     assert_email_delivered_with(subject: "pirates")
   end
 
   # Statutory edited
   test "POST / statutory edited", %{conn: conn} do
-    conn = post conn, "/", %{template: "statutory_edited.html", parameters: %{event: %{name: "test" }, application: %{}}, to: "test@aegee.org", subject: "pirates"}
+    conn = post conn, "/", %{template: "statutory_edited.html", parameters: %{event: %{name: "test", "questions": []}, application: %{}}, to: "test@aegee.org", subject: "pirates"}
     assert json_response(conn, 200)
     assert_email_delivered_with(subject: "pirates")
   end
