88e6092b4f0cfae19b6963a44b0e0e7a8fcf1c4f
Sergey Peshkov <serge1peshcoff@mail.ru>
2019-02-08 17:19:23 +0300

    chore(stats): right sorting for statutory statistics

--- statutory/test/api/statistics.test.js
+++ statutory/test/api/statistics.test.js
@@ -178,6 +178,102 @@ describe('Statistics testing', () => {
         expect(res.body.data.by_number_of_events_visited.find(t => t.type === 1).value).toEqual(1);
     });
 
+    test('should sort properly by body', async () => {
+        await generator.createApplication({ user_id: 1, body_id: 1, participant_type: null, participant_order: null }, event);
+        await generator.createApplication({ user_id: 2, body_id: 3, participant_type: null, participant_order: null }, event);
+        await generator.createApplication({ user_id: 3, body_id: 2, participant_type: null, participant_order: null }, event);
+        await generator.createApplication({ user_id: 4, body_id: 2, participant_type: null, participant_order: null }, event);
+        await generator.createApplication({ user_id: 5, body_id: 3, participant_type: null, participant_order: null }, event);
+        await generator.createApplication({ user_id: 6, body_id: 3, participant_type: null, participant_order: null }, event);
+
+        const res = await request({
+            uri: '/events/' + event.id + '/applications/stats',
+            method: 'GET',
+            headers: { 'X-Auth-Token': 'blablabla' }
+        });
+
+        expect(res.statusCode).toEqual(200);
+        expect(res.body.success).toEqual(true);
+        expect(res.body).not.toHaveProperty('errors');
+        expect(res.body).toHaveProperty('data');
+
+        expect(Object.keys(res.body.data.by_body).length).toEqual(3);
+        expect(res.body.data.by_body[0].type).toEqual(3); // body_id 3, 3 persons applied
+        expect(res.body.data.by_body[1].type).toEqual(2); // body_id 2, 2 persons applied
+        expect(res.body.data.by_body[2].type).toEqual(1); // body_id 1, 1 person applied
+    });
+
+    test('should sort properly by gender', async () => {
+        await generator.createApplication({ user_id: 1, gender: 'male' }, event);
+        await generator.createApplication({ user_id: 2, gender: 'other' }, event);
+        await generator.createApplication({ user_id: 3, gender: 'male' }, event);
+        await generator.createApplication({ user_id: 4, gender: 'female' }, event);
+        await generator.createApplication({ user_id: 5, gender: 'female' }, event);
+        await generator.createApplication({ user_id: 6, gender: 'female' }, event);
+
+        const res = await request({
+            uri: '/events/' + event.id + '/applications/stats',
+            method: 'GET',
+            headers: { 'X-Auth-Token': 'blablabla' }
+        });
+
+        expect(res.statusCode).toEqual(200);
+        expect(res.body.success).toEqual(true);
+        expect(res.body).not.toHaveProperty('errors');
+        expect(res.body).toHaveProperty('data');
+
+        expect(Object.keys(res.body.data.by_gender).length).toEqual(3);
+        expect(res.body.data.by_gender[0].type).toEqual('female'); // female, 3 persons applied
+        expect(res.body.data.by_gender[1].type).toEqual('male'); // male, 2 persons applied
+        expect(res.body.data.by_gender[2].type).toEqual('other'); // other, 1 person applied
+    });
+
+    test('should sort properly by participant type', async () => {
+        await generator.createApplication({ user_id: 1, participant_type: 'envoy', participant_order: 1 }, event);
+        await generator.createApplication({ user_id: 2, participant_type: null, participant_order: null }, event);
+        await generator.createApplication({ user_id: 3, participant_type: 'envoy', participant_order: 2 }, event);
+        await generator.createApplication({ user_id: 4, participant_type: 'visitor', participant_order: 3 }, event);
+        await generator.createApplication({ user_id: 5, participant_type: 'visitor', participant_order: 4 }, event);
+        await generator.createApplication({ user_id: 6, participant_type: 'visitor', participant_order: 5 }, event);
+
+        const res = await request({
+            uri: '/events/' + event.id + '/applications/stats',
+            method: 'GET',
+            headers: { 'X-Auth-Token': 'blablabla' }
+        });
+
+        expect(res.statusCode).toEqual(200);
+        expect(res.body.success).toEqual(true);
+        expect(res.body).not.toHaveProperty('errors');
+        expect(res.body).toHaveProperty('data');
+
+        expect(Object.keys(res.body.data.by_type).length).toEqual(3);
+        expect(res.body.data.by_type[0].type).toEqual('visitor'); // visitor, 3 persons applied
+        expect(res.body.data.by_type[1].type).toEqual('envoy'); // envoy, 2 persons applied
+        expect(res.body.data.by_type[2].type).toEqual(null); // no pax type set, 1 person applied
+    });
+
+    test('should sort properly by number of events visited', async () => {
+        await generator.createApplication({ user_id: 1, number_of_events_visited: 3 }, event);
+        await generator.createApplication({ user_id: 2, number_of_events_visited: 1 }, event);
+        await generator.createApplication({ user_id: 3, number_of_events_visited: 2 }, event);
+
+        const res = await request({
+            uri: '/events/' + event.id + '/applications/stats',
+            method: 'GET',
+            headers: { 'X-Auth-Token': 'blablabla' }
+        });
+
+        expect(res.statusCode).toEqual(200);
+        expect(res.body.success).toEqual(true);
+        expect(res.body).not.toHaveProperty('errors');
+        expect(res.body).toHaveProperty('data');
+        expect(Object.keys(res.body.data.by_number_of_events_visited).length).toEqual(3);
+        expect(res.body.data.by_number_of_events_visited[0].type).toEqual(1);
+        expect(res.body.data.by_number_of_events_visited[1].type).toEqual(2);
+        expect(res.body.data.by_number_of_events_visited[2].type).toEqual(3);
+    });
+
     test('should calculate numbers properly', async () => {
         await generator.createApplication({ user_id: 1, status: 'pending' }, event);
         await generator.createApplication({ user_id: 2, status: 'rejected' }, event);
