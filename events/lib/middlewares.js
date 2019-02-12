d623a752153ca783c62455efcdaa584508fe9f3b
Sergey Peshkov <serge1peshcoff@mail.ru>
2019-02-12 12:03:22 +0300

    fix(middlewares): refactor fetching core data

--- events/lib/middlewares.js
+++ events/lib/middlewares.js
@@ -13,110 +13,90 @@ exports.authenticateUser = async (req, res, next) => {
         return errors.makeError(res, 401, 'No auth token provided');
     }
 
-    try {
     // Query the core for user and permissions.
-        const [userBody, permissionsBody] = await Promise.all(['members/me', 'my_permissions'].map(endpoint => request({
-            url: config.core.url + ':' + config.core.port + '/' + endpoint,
-            method: 'GET',
-            headers: {
-                'X-Requested-With': 'XMLHttpRequest',
-                'X-Auth-Token': req.headers['x-auth-token'],
-            },
-            simple: false,
-            json: true,
-        })));
+    const [userBody, permissionsBody] = await Promise.all(['members/me', 'my_permissions'].map(endpoint => request({
+        url: config.core.url + ':' + config.core.port + '/' + endpoint,
+        method: 'GET',
+        headers: {
+            'X-Requested-With': 'XMLHttpRequest',
+            'X-Auth-Token': req.headers['x-auth-token'],
+        },
+        simple: false,
+        json: true,
+    })));
 
-        if (typeof userBody !== 'object') {
-            throw new Error('Malformed response when fetching user: ' + userBody);
-        }
+    if (typeof userBody !== 'object') {
+        throw new Error('Malformed response when fetching user: ' + userBody);
+    }
 
-        if (!userBody.success) {
-      // We are not authenticated
-            return errors.makeUnauthorizedError(res, 'Error fetching user: user is not authenticated.');
-        }
+    if (!userBody.success) {
+        // We are not authenticated
+        return errors.makeUnauthorizedError(res, 'Error fetching user: user is not authenticated.');
+    }
 
-        if (typeof permissionsBody !== 'object') {
-            throw new Error('Malformed response when fetching permissions: ' + JSON.stringify(permissionsBody));
-        }
+    if (typeof permissionsBody !== 'object') {
+        throw new Error('Malformed response when fetching permissions: ' + JSON.stringify(permissionsBody));
+    }
 
-        if (!permissionsBody.success) {
-      // We are not authenticated
-            return errors.makeUnauthorizedError(res, 'Error fetching permissions: user is not authenticated.');
-        }
+    if (!permissionsBody.success) {
+        throw new Error('Error fetching permissions: ' +  + JSON.stringify(permissionsBody));
+    }
 
     // Fetching permissions for members approval, the list of bodies
     // where do you have the 'approve_members:events' permission for it.
-        const approveRequest = await request({
-            url: config.core.url + ':' + config.core.port + '/my_permissions',
-            method: 'POST',
-            headers: {
-                'X-Requested-With': 'XMLHttpRequest',
-                'X-Auth-Token': req.headers['x-auth-token'],
-            },
-            simple: false,
-            json: true,
-            body: {
-                action: 'approve_members',
-                object: 'events'
-            }
-        });
-
-        if (typeof approveRequest !== 'object') {
-            throw new Error('Malformed response when fetching permissions for approve: ' + approveRequest);
+    const approveRequest = await request({
+        url: config.core.url + ':' + config.core.port + '/my_permissions',
+        method: 'POST',
+        headers: {
+            'X-Requested-With': 'XMLHttpRequest',
+            'X-Auth-Token': req.headers['x-auth-token'],
+        },
+        simple: false,
+        json: true,
+        body: {
+            action: 'approve_members',
+            object: 'events'
         }
+    });
 
-        if (!approveRequest.success) {
-        // We are not authenticated
-            throw new Error('Error fetching permissions for approve: user is not authenticated.');
-        }
-
-        req.user = userBody.data;
-        req.corePermissions = permissionsBody.data;
-        req.approvePermissions = approveRequest.data;
-        req.permissions = helpers.getPermissions(req.user, req.corePermissions, req.approvePermissions);
-
-        return next();
-    } catch (err) {
-        return errors.makeInternalError(res, err);
+    if (typeof approveRequest !== 'object') {
+        throw new Error('Malformed response when fetching permissions for approve: ' + JSON.stringify(approveRequest));
     }
+
+    if (!approveRequest.success) {
+        throw new Error('Error fetching permissions for approve:' + JSON.stringify(approveRequest));
+    }
+
+    req.user = userBody.data;
+    req.corePermissions = permissionsBody.data;
+    req.approvePermissions = approveRequest.data;
+    req.permissions = helpers.getPermissions(req.user, req.corePermissions, req.approvePermissions);
+
+    return next();
 };
 
 exports.fetchSingleEvent = async (req, res, next) => {
-    if (!req.params.event_id) {
-        logger.info(req.params);
-        return errors.makeNotFoundError(res, 'No Event-id provided');
-    }
-
-  // Checking if the passed ID is ObjectID or not.
-  // We don't use ObjectID.isValid method, since it's not always
-  // working properly, see http://stackoverflow.com/a/29231016/1206421
-    let findObject;
-    if (!Number.isNaN(Number(req.params.event_id))) { // if it's indeed an ObjectID
+    // Checking if the passed ID is a string or not.
+    // If it is a string, find the event by URL, if not, find it by ID.
+    let findObject = { url: req.params.event_id };
+    if (!Number.isNaN(Number(req.params.event_id))) {
         findObject = { id: Number(req.params.event_id) };
-    } else {
-        findObject = { url: req.params.event_id };
     }
 
-    try {
-        const event = await Event.findOne({ where: findObject });
+    const event = await Event.findOne({ where: findObject });
 
-        if (!event) {
-            return errors.makeNotFoundError(res, `Event with id ${req.params.event_id} not found`);
-        }
-
-
-        req.event = event;
-        req.permissions = helpers.getEventPermissions({
-            permissions: req.permissions,
-            corePermissions: req.corePermissions,
-            user: req.user,
-            event
-        });
-        return next();
-    } catch (err) {
-        logger.error('Error getting single event: ', err);
-        throw err;
+    if (!event) {
+        return errors.makeNotFoundError(res, `Event with id ${req.params.event_id} not found`);
     }
+
+    req.event = event;
+    req.permissions = helpers.getEventPermissions({
+        permissions: req.permissions,
+        corePermissions: req.corePermissions,
+        user: req.user,
+        event
+    });
+    return next();
 };
 
 exports.fetchSingleApplication = async (req, res, next) => {
