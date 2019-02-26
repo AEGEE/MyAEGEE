7f027ff02ed13f3ac7ea60aaba612f6457f29cc0
Sergey Peshkov <serge1peshcoff@mail.ru>
2019-02-26 13:28:08 +0300

    fix(memberslist): allow null values as user IDs for memberslist

--- statutory/models/MembersList.js
+++ statutory/models/MembersList.js
@@ -81,7 +81,7 @@ const MembersList = sequelize.define('memberslist', {
                         }
                     }
 
-                    if (typeof member.user_id !== 'undefined' && typeof member.user_id !== 'number') {
+                    if (typeof member.user_id !== 'undefined' && member.user_id !== null && typeof member.user_id !== 'number') {
                         throw new Error('user_id is set, but is not a number.');
                     }
 
