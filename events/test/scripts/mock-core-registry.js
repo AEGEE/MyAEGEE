9aeb9c4fa1ae720616807571a32c0f6e35c34c26
Sergey Peshkov <serge1peshcoff@mail.ru>
2019-03-13 19:31:57 +0300

    feat(event): fix organizers management. Fixes MEMB-191

--- events/test/scripts/mock-core-registry.js
+++ events/test/scripts/mock-core-registry.js
@@ -2,6 +2,7 @@ const nock = require('nock');
 const path = require('path');
 
 const config = require('../../config');
+const regularUser = require('../assets/oms-core-valid.json').data;
 
 exports.cleanAll = () => nock.cleanAll();
 
@@ -133,15 +134,46 @@ exports.mockCoreApprovePermissions = (options) => {
         .replyWithFile(200, path.join(__dirname, '..', 'assets', 'oms-core-approve-permissions-full.json'));
 };
 
+
+exports.mockCoreMember = (options) => {
+    if (options.netError) {
+        return nock(`${config.core.url}:${config.core.port}`)
+            .persist()
+            .get(/\/members\/[0-9].*/)
+            .replyWithError('Some random error.');
+    }
+
+    if (options.badResponse) {
+        return nock(`${config.core.url}:${config.core.port}`)
+            .persist()
+            .get(/\/members\/[0-9].*/)
+            .reply(500, 'Some error happened.');
+    }
+
+    if (options.unsuccessfulResponse) {
+        return nock(`${config.core.url}:${config.core.port}`)
+            .persist()
+            .get(/\/members\/[0-9].*/)
+            .reply(500, { success: false, message: 'Some error' });
+    }
+
+    return nock(`${config.core.url}:${config.core.port}`)
+        .persist()
+        .get(/\/members\/[0-9].*/)
+        .reply(200, { success: true, data: regularUser });
+};
+
 exports.mockAll = (options = {}) => {
     nock.cleanAll();
     const omscoreStub = exports.mockCore(options.core || {});
     const omsMainPermissionsStub = exports.mockCoreMainPermissions(options.mainPermissions || {});
     const omsApprovePermissionsStub = exports.mockCoreApprovePermissions(options.approvePermissions || {});
+    const omsCoreMemberStub = exports.mockCoreMember(options.member || {});
 
     return {
         omscoreStub,
         omsMainPermissionsStub,
-        omsApprovePermissionsStub
+        omsApprovePermissionsStub,
+        omsCoreMemberStub
     };
 };
