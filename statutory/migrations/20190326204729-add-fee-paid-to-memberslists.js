233630d1ec78d379f69aa37f4dad845ed310128a
Sergey Peshkov <serge1peshcoff@mail.ru>
2019-03-30 16:52:34 +0300

    style: eslint compliance

--- statutory/migrations/20190326204729-add-fee-paid-to-memberslists.js
+++ statutory/migrations/20190326204729-add-fee-paid-to-memberslists.js
@@ -1,8 +1,8 @@
 module.exports = {
-  up: (queryInterface, Sequelize) => queryInterface.addColumn(
-      'memberslists',
-      'fee_paid',
-      { type: Sequelize.DECIMAL, allowNull: false, defaultValue: 0 }
-  ),
-  down: queryInterface => queryInterface.removeColumn('memberslists', 'fee_paid')
+    up: (queryInterface, Sequelize) => queryInterface.addColumn(
+        'memberslists',
+        'fee_paid',
+        { type: Sequelize.DECIMAL, allowNull: false, defaultValue: 0 }
+    ),
+    down: queryInterface => queryInterface.removeColumn('memberslists', 'fee_paid')
 };
