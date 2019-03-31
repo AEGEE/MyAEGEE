c0d33e7058b7658b37f6c5b1ed3d22bc8bb91cae
Sergey Peshkov <serge1peshcoff@mail.ru>
2019-03-31 22:32:48 +0300

    fix(statutory): proper sorting on participants list. Fixes HELP-430

--- frontend/client/views/statutory/Accepted.vue
+++ frontend/client/views/statutory/Accepted.vue
@@ -12,21 +12,21 @@
           default-sort="id"
           default-sort-direction="desc">
           <template slot-scope="props">
-            <b-table-column field="id" label="#" numeric sortable>
+            <b-table-column field="id" label="#" numeric>
               {{ props.row.id }}
             </b-table-column>
 
-            <b-table-column field="first_name" label="Name" sortable>
+            <b-table-column field="first_name" label="Name">
               {{ props.row.first_name }} {{ props.row.last_name }}
             </b-table-column>
 
-            <b-table-column field="body_name" label="Body" sortable>
+            <b-table-column field="body_name" label="Body">
               <router-link :to="{ name: 'oms.bodies.view', params: { id: props.row.body_id } }">
                   {{ props.row.body_name }}
                 </router-link>
             </b-table-column>
 
-            <b-table-column field="participant_type" label="Participant type and order" centered sortable>
+            <b-table-column field="participant_type" label="Participant type and order">
               {{ props.row.participant_type ? `${props.row.participant_type} (${props.row.participant_order})` : '' }}
             </b-table-column>
           </template>
@@ -65,7 +65,19 @@ export default {
   mounted () {
     this.isLoading = true
     this.axios.get(this.services['oms-statutory'] + '/events/' + this.$route.params.id + '/applications/accepted').then((application) => {
-      this.applications = application.data.data
+      this.applications = application.data.data.sort((a, b) => {
+        const byBody = a.body_name.localeCompare(b.body_name)
+        if (byBody !== 0) {
+          return byBody
+        }
+
+        const byParticipantType = (a.participant_type || 'unset').localeCompare(b.participant_type || 'unset')
+        if (byParticipantType !== 0) {
+          return byParticipantType
+        }
+
+        return (a.participant_order || 0) - (b.participant_order || 0)
+      })
       this.isLoading = false
     }).catch((err) => {
       let message = (err.response && err.response.status === 404) ? 'Event is not found' : 'Some error happened: ' + err.message
