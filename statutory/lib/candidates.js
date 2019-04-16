c8cd5f6c178301ed0c8d2f5674dc7dae4c22ea92
Sergey Peshkov <serge1peshcoff@mail.ru>
2019-04-16 20:46:11 +0300

    fix(candidates): do not update position on status change. Fixed MEMB-519

--- statutory/lib/candidates.js
+++ statutory/lib/candidates.js
@@ -96,21 +96,6 @@ exports.setCandidatureStatus = async (req, res) => {
 
     await req.candidate.update({ status: req.body.status });
 
-    // Checking if we have enough approve candidates and if the deadline has passed.
-    // If so, closing the deadline (can reopen manually later).
-    const candidatesCount = await Candidate.count({
-        where: {
-            position_id: req.position.id,
-            status: { [Sequelize.Op.ne]: 'rejected' }
-        }
-    });
-
-    if (moment().isAfter(req.position.ends)) {
-        await req.position.update({
-            status: candidatesCount > req.position.places ? 'closed' : 'open'
-        }, { hooks: false });
-    }
-
     return res.json({
         success: true,
         data: req.candidate
