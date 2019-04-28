f703713c4e647b586f4c68434284a263ced7baf9
Sergey Peshkov <serge1peshcoff@mail.ru>
2019-04-28 18:20:45 +0300

    feat(code): add code model

--- discounts/models/index.js
+++ discounts/models/index.js
@@ -1,5 +1,10 @@
 const Integration = require('./Integration');
+const Code = require('./Code');
+
+Integration.hasMany(Code);
+Code.belongsTo(Integration);
 
 module.exports = {
-    Integration
+    Integration,
+    Code
 };
