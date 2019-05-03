bf73ec5ef76294581450332b1fe1903af1beef6c
Sergey Peshkov <serge1peshcoff@mail.ru>
2019-05-04 01:24:59 +0300

    fix(codes): fix claimed date

--- discounts/lib/helpers.js
+++ discounts/lib/helpers.js
@@ -24,7 +24,7 @@ You've claimed the code for the discount, here are the details.<br/>
 <br/>
 Partner: ${integration.name}<br/>
 Code: ${code.value}<br/>
-Claimed on: ${moment(code.created_at).format('YYYY-MM-DD HH:MM')}<br/>
+Claimed on: ${moment(code.updated_at).format('YYYY-MM-DD HH:MM')}<br/>
 <br/>
 ${integration.description}<br/>
 <br/>
