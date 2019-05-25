5e933fd94d79246369c6ea32fe88380f6f4429d9
Sergey Peshkov <serge1peshcoff@mail.ru>
2019-05-25 21:21:29 +0300

    style: eslint compliance

--- statutory/migrations/20190525103335-add-statutory-id-to-applications.js
+++ statutory/migrations/20190525103335-add-statutory-id-to-applications.js
@@ -1,22 +1,22 @@
 module.exports = {
-  up: async (queryInterface, Sequelize) => {
-    await queryInterface.addColumn(
-      'applications',
-      'statutory_id',
-      {
-        type: Sequelize.STRING
-      }
-    );
-    await queryInterface.sequelize.query('update applications set statutory_id = LPAD(event_id::text, 3, \'0\') || \'-\' || LPAD(id::text, 4, \'0\');');
-    await queryInterface.changeColumn(
-      'applications',
-      'statutory_id',
-      {
-        type: Sequelize.STRING,
-        allowNull: false,
-        unique: true
-      }
-    );
-  },
-  down: queryInterface => queryInterface.removeColumn('applications', 'statutory_id')
+    up: async (queryInterface, Sequelize) => {
+        await queryInterface.addColumn(
+            'applications',
+            'statutory_id',
+            {
+                type: Sequelize.STRING
+            }
+        );
+        await queryInterface.sequelize.query('update applications set statutory_id = LPAD(event_id::text, 3, \'0\') || \'-\' || LPAD(id::text, 4, \'0\');');
+        await queryInterface.changeColumn(
+            'applications',
+            'statutory_id',
+            {
+                type: Sequelize.STRING,
+                allowNull: false,
+                unique: true
+            }
+        );
+    },
+    down: queryInterface => queryInterface.removeColumn('applications', 'statutory_id')
 };
