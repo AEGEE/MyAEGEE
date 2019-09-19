d682890f36b08c7e194b435524ce353d3bcd1154
Sergey Peshkov <serge1peshcoff@mail.ru>
2019-09-20 00:58:22 +0300

    fix(metrics): reduced metrics count and refactor

--- statutory/lib/metrics.js
+++ statutory/lib/metrics.js
@@ -12,54 +12,37 @@ const {
 } = require('../models');
 const helpers = require('./helpers');
 
-const createGauge = (name, help, labels = []) => new Gauge({
-    name,
-    help,
-    labelNames: labels
-});
-
 const gaugesList = {
-    eventsTotal: createGauge('statutory_events_total', 'Total amount of statutory events'),
-    eventsByType: createGauge('statutory_events_by_type', 'Amount of statutory events by type', ['type']),
-    eventsByTypeAndStatus: createGauge(
-        'statutory_events_by_type_and_status',
-        'Amount of statutory events by type and status',
-        ['type', 'status']
-    ),
-    applicationsTotal: createGauge('statutory_applications_total', 'Total amount of statutory applications'),
-    applicationsByEvent: createGauge('statutory_applications_by_event', 'Amount of statutory applications by event', ['event_name']),
-    applicationsByEventAndBody: createGauge(
-        'statutory_applications_by_event_and_body',
-        'Amount of statutory applications by event and body',
-        ['event_name', 'body_name']
-    ),
-    applicationsByEventAndPaxType: createGauge(
-        'statutory_applications_by_event_and_pax_type',
-        'Amount of statutory applications by event and pax type',
-        ['event_name', 'participant_type']
-    ),
-    memberslistsTotal: createGauge('statutory_memberslists_total', 'Total amount of statutory memberslists'),
-    memberslistsByEvent: createGauge('statutory_memberslists_by_event', 'Amount of statutory memberslists by event', ['event_name']),
-    memberslistsByEventAndBody: createGauge(
-        'statutory_memberslists_by_event_and_body',
-        'Amount of statutory memberslists by event and body',
-        ['event_name', 'body_id']
-    ),
-    positionsTotal: createGauge('statutory_positions_total', 'Total amount of statutory positions'),
-    positionsByEvent: createGauge('statutory_positions_by_event', 'Amount of statutory positions by event', ['event_name']),
-    candidatesTotal: createGauge('statutory_candidates_total', 'Total amount of statutory candidates'),
-    candidatesByStatus: createGauge('statutory_candidates_by_status', 'Amount of statutory candidates by status', ['status']),
-    candidatesByEventAndPosition: createGauge(
-        'statutory_candidates_by_event_and_position',
-        'Amount of statutory candidates by event and position',
-        ['event_name', 'position_name']
-    ),
-    candidatesByStatusAndPosition: createGauge(
-        'statutory_candidates_by_status_and_position',
-        'Amount of statutory candidates by status and position',
-        ['status', 'position_name', 'event_name']
-    ),
-
+    eventsTotal: new Gauge({
+        name: 'statutory_events_total',
+        help: 'Total amount of statutory events',
+        labelNames: ['type', 'status']
+    }),
+    applicationsTotal: new Gauge({
+        name: 'statutory_applications_total',
+        help: 'Total amount of statutory applications',
+        labelNames: ['event_name', 'body_name', 'participant_type']
+    }),
+    memberslistsTotal: new Gauge({
+        name: 'statutory_memberslists_total',
+        help: 'Total amount of statutory memberslists',
+        labelNames: ['event_name', 'body_name']
+    }),
+    memberslistsByEventAndBody: new Gauge({
+        name: 'statutory_memberslists_by_event_and_body',
+        help: 'Amount of memberslists by event body',
+        labelNames: ['event_name', 'body_id']
+    }),
+    positionsTotal: new Gauge({
+        name: 'statutory_positions_total',
+        help: 'Total amount of statutory positions',
+        labelNames: ['event_name']
+    }),
+    candidatesTotal: new Gauge({
+        name: 'statutory_candidates_total',
+        help: 'Total amount of statutory candidates',
+        labelNames: ['status', 'position_name', 'event_name']
+    })
 };
 
 exports.getMetrics = async (req, res) => {
@@ -89,7 +72,6 @@ exports.getMetrics = async (req, res) => {
         position_name: candidate.position.name
     }));
 
-
     const memberslistsByBody = memberslists.map(memberslist => ({
         event_name: memberslist.event_name,
         body_id: memberslist.body_id,
@@ -97,26 +79,12 @@ exports.getMetrics = async (req, res) => {
     }));
 
     // setting gauges with real data
-    helpers.addGaugeData(gaugesList.eventsTotal, helpers.countByFields(events, []));
-    helpers.addGaugeData(gaugesList.eventsByType, helpers.countByFields(events, ['type']));
-    helpers.addGaugeData(gaugesList.eventsByTypeAndStatus, helpers.countByFields(events, ['type', 'status']));
-
-    helpers.addGaugeData(gaugesList.applicationsTotal, helpers.countByFields(applications, []));
-    helpers.addGaugeData(gaugesList.applicationsByEvent, helpers.countByFields(applications, ['event_name']));
-    helpers.addGaugeData(gaugesList.applicationsByEventAndBody, helpers.countByFields(applications, ['event_name', 'body_name']));
-    helpers.addGaugeData(gaugesList.applicationsByEventAndPaxType, helpers.countByFields(applications, ['event_name', 'participant_type']));
-
-    helpers.addGaugeData(gaugesList.memberslistsTotal, helpers.countByFields(memberslists, []));
-    helpers.addGaugeData(gaugesList.memberslistsByEvent, helpers.countByFields(memberslists, ['event_name']));
+    helpers.addGaugeData(gaugesList.eventsTotal, helpers.countByFields(events, ['type', 'status']));
+    helpers.addGaugeData(gaugesList.applicationsTotal, helpers.countByFields(applications, ['event_name', 'body_name', 'participant_type']));
+    helpers.addGaugeData(gaugesList.memberslistsTotal, helpers.countByFields(memberslists, ['event_name']));
     helpers.addGaugeData(gaugesList.memberslistsByEventAndBody, memberslistsByBody);
-
-    helpers.addGaugeData(gaugesList.positionsTotal, helpers.countByFields(positions, []));
-    helpers.addGaugeData(gaugesList.positionsByEvent, helpers.countByFields(positions, ['event_name']));
-
-    helpers.addGaugeData(gaugesList.candidatesTotal, helpers.countByFields(candidates, []));
-    helpers.addGaugeData(gaugesList.candidatesByStatus, helpers.countByFields(candidates, ['status']));
-    helpers.addGaugeData(gaugesList.candidatesByEventAndPosition, helpers.countByFields(candidates, ['position_name', 'event_name']));
-    helpers.addGaugeData(gaugesList.candidatesByStatusAndPosition, helpers.countByFields(candidates, ['status', 'position_name', 'event_name']));
+    helpers.addGaugeData(gaugesList.positionsTotal, helpers.countByFields(positions, ['event_name']));
+    helpers.addGaugeData(gaugesList.candidatesTotal, helpers.countByFields(candidates, ['status', 'position_name', 'event_name']));
 
     res.set('Content-Type', register.contentType);
     res.end(register.metrics());
