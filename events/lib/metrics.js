ce6bf75ef697276445302b3e48600ddcd34513a8
Sergey Peshkov <serge1peshcoff@mail.ru>
2019-09-20 01:05:24 +0300

    fix(metrics): reduced metrics count and refactor

--- events/lib/metrics.js
+++ events/lib/metrics.js
@@ -9,32 +9,17 @@ const {
 } = require('../models');
 const helpers = require('./helpers');
 
-const createGauge = (name, help, labels = []) => new Gauge({
-    name,
-    help,
-    labelNames: labels
-});
-
 const gaugesList = {
-    eventsTotal: createGauge('events_events_total', 'Total amount of general events'),
-    eventsByType: createGauge('events_events_by_type', 'Amount of general events by type', ['type']),
-    eventsByTypeAndStatus: createGauge(
-        'events_events_by_type_and_status',
-        'Amount of general events by type and status',
-        ['type', 'status']
-    ),
-    applicationsTotal: createGauge('events_applications_total', 'Total amount of general events applications'),
-    applicationsByEvent: createGauge('events_applications_by_event', 'Amount of general events applications by event', ['event_name']),
-    applicationsByEventAndStatus: createGauge(
-        'events_applications_by_event_and_status',
-        'Amount of general events applications by event and status',
-        ['event_name', 'status']
-    ),
-    applicationsByEventAndBody: createGauge(
-        'events_applications_by_event_and_body',
-        'Amount of general events applications by event and body',
-        ['event_name', 'body_name']
-    ),
+    eventsTotal: new Gauge({
+        name: 'events_events_total',
+        help: 'Total amount of general events',
+        labelNames: ['type', 'status']
+    }),
+    applicationsTotal: new Gauge({
+        name: 'events_applications_total',
+        help: 'Total amount of general events applications',
+        labelNames: ['event_name', 'status', 'body_name']
+    }),
 };
 
 exports.getMetrics = async (req, res) => {
@@ -50,14 +35,8 @@ exports.getMetrics = async (req, res) => {
     applications = applications.map((application) => Object.assign(application.toJSON(), { event_name: application.event.name }));
 
     // setting gauges with real data
-    helpers.addGaugeData(gaugesList.eventsTotal, helpers.countByFields(events, []));
-    helpers.addGaugeData(gaugesList.eventsByType, helpers.countByFields(events, ['type']));
-    helpers.addGaugeData(gaugesList.eventsByTypeAndStatus, helpers.countByFields(events, ['type', 'status']));
-
-    helpers.addGaugeData(gaugesList.applicationsTotal, helpers.countByFields(applications, []));
-    helpers.addGaugeData(gaugesList.applicationsByEvent, helpers.countByFields(applications, ['event_name']));
-    helpers.addGaugeData(gaugesList.applicationsByEventAndBody, helpers.countByFields(applications, ['event_name', 'body_name']));
-    helpers.addGaugeData(gaugesList.applicationsByEventAndStatus, helpers.countByFields(applications, ['event_name', 'status']));
+    helpers.addGaugeData(gaugesList.eventsTotal, helpers.countByFields(events, ['type', 'status']));
+    helpers.addGaugeData(gaugesList.applicationsTotal, helpers.countByFields(applications, ['event_name', 'body_name', 'status']));
 
     res.set('Content-Type', register.contentType);
     res.end(register.metrics());
