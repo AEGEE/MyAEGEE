ad49533aee96776b4c9c81899e3f8451390da0bb
grasshopper <bellicaf@tcd.ie>
2019-09-28 18:29:16 +0200

    impr(vagrant): bump ubuntu to 18.04 and docker
    Docker daemon: 18.09.4
    Docker-compose:1.24.0

--- vagrant-post-script/install_docker.sh
+++ vagrant-post-script/install_docker.sh
@@ -6,23 +6,13 @@ echo "[Vagrant] ###################     Installing docker"
 
 sudo apt-get update > /dev/null
 
-sudo apt-get install -qq -y \
-    apt-transport-https \
-    ca-certificates \
-    curl \
-    software-properties-common
-
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 
 sudo apt-key fingerprint 0EBFCD88 || { echo "[Vagrant] ###################     Fingerprint error, exiting"; exit; }
 
-sudo add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-   $(lsb_release -cs) \
-   stable"
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
 
 sudo apt-get update > /dev/null
-sudo apt-get install -qq -y docker-ce=17.06.0~ce-0~ubuntu || { echo "[Vagrant] ###################     Installation error, exiting"; exit; }
+sudo apt-get install -qq -y docker-ce=5:18.09.4~3-0~ubuntu-bionic || { echo "[Vagrant] ###################     Installation error, exiting"; exit; }
 
 echo "[Vagrant] ###################     Installation complete"
-
--- vagrant-post-script/install_docker_composer.sh
+++ vagrant-post-script/install_docker_composer.sh
@@ -6,9 +6,7 @@ echo "[Vagrant] ###################     Installing docker-composer from script f
 
 if [ -a /usr/local/bin/docker-compose ]; then echo "[Vagrant] ###################     docker-compose already installed, exiting"; exit; fi
 
-#curl -L https://github.com/docker/compose/releases/download/1.9.0/docker-compose-$(uname -s)-$(uname -m) > ~/docker-compose
-curl -sSL "https://github.com/docker/compose/releases/download/1.16.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+curl -L "https://github.com/docker/compose/releases/download/1.24.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 
-#sudo mv ~/docker-compose /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
