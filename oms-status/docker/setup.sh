d15334213c48ac33c143cf05ae48092f109f4c14
grasshopper <bellicaf@tcd.ie>
2019-10-17 19:47:04 +0200

    fix: change targets and paths

--- oms-status/docker/setup.sh
+++ oms-status/docker/setup.sh
@@ -4,6 +4,8 @@ if [[ ! -f ./cachet/.env ]]; then
   #export PHP_APP_KEY=$(cat ../../.env | grep PHP_APP_KEY | awk -F= '{print $2}' )
   export $(cat ../../.env | grep -v ^# | xargs)
   envsubst < ./cachet/.env.example > ./cachet/.env
+else
+  echo "file exists already"
 fi
 
-chown 82 ./cachet/.env
+sudo chown 82 ./cachet/.env
