a7217c059b582f2abc7036e84ce37019c78ffa30
Sergey Peshkov <sergeypeshkov@yandex-team.ru>
2019-11-04 04:18:58 +0300

    chore: renamed var to fix stuff

--- discounts/lib/bugsnag.js
+++ discounts/lib/bugsnag.js
@@ -2,12 +2,12 @@ const bugsnag = require('@bugsnag/js');
 
 const config = require('../config');
 const logger = require('./logger');
-const package = require('../package.json');
+const packageInfo = require('../package.json');
 
 const bugsnagClient = bugsnag({
     apiKey: config.bugsnagKey,
     logger,
-    appVersion: package.version,
+    appVersion: packageInfo.version,
     hostname: config.host,
     releaseStage: process.env.NODE_ENV
 });
