6ee521486c7087837cc6dfe22017dcd5cb20992f
Sergey Peshkov <sergeypeshkov@yandex-team.ru>
2019-11-04 04:12:14 +0300

    fix(bugsnag): add bugsnag params. Fixes MEMB-642

--- discounts/lib/bugsnag.js
+++ discounts/lib/bugsnag.js
@@ -2,10 +2,14 @@ const bugsnag = require('@bugsnag/js');
 
 const config = require('../config');
 const logger = require('./logger');
+const package = require('../package.json');
 
 const bugsnagClient = bugsnag({
     apiKey: config.bugsnagKey,
-    logger
+    logger,
+    appVersion: package.version,
+    hostname: config.host,
+    releaseStage: process.env.NODE_ENV
 });
 
 module.exports = bugsnagClient;
