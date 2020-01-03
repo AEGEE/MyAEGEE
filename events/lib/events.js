310ff7ca4ff2e38f908781cf0bd2d55f016c8a56
Sergey Peshkov <serge1peshcoff@gmail.com>
2020-01-03 12:46:11 +0300

    chore: eslint compliance

--- events/lib/events.js
+++ events/lib/events.js
@@ -234,7 +234,7 @@ exports.setApprovalStatus = async (req, res) => {
 
     // Send email to all organizers.
     await mailer.sendMail({
-        to: req.event.organizers.map(organizer => organizer.email),
+        to: req.event.organizers.map((organizer) => organizer.email),
         subject: 'Your event\'s status was changed',
         template: 'events_status_changed.html',
         parameters: {
