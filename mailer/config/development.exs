7fb70ae128e73ba4086c33fb27a83156002c6f59
Sergey Peshkov <serge1peshcoff@gmail.com>
2020-01-10 17:08:22 +0300

    feat(general): event submitted template for EQAC. Fixes MEMB-759

--- mailer/config/development.exs
+++ mailer/config/development.exs
@@ -1,1 +1,9 @@
-# Nothing to see here
+use Mix.Config
+
+config :omsmailer, OmsmailerWeb.Endpoint,
+  load_from_system_env: true
+
+config :logger, level: :debug
+
+config :omsmailer, OmsmailerWeb.Endpoint,
+  secret_key_base: "test"
