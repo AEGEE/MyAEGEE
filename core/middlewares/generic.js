62a41e386cf1183734f865824a86d64cb5da3db3
Sergey Peshkov <serge1peshcoff@gmail.com>
2020-02-11 18:36:51 +0300

    feat(general): refactored fetch methods

--- core/middlewares/generic.js
+++ core/middlewares/generic.js
@@ -6,7 +6,6 @@ const logger = require('../lib/logger');
 const helpers = require('../lib/helpers');
 const {
     User,
-    Body,
     Circle,
     CircleMembership,
     CirclePermission,
@@ -86,100 +85,6 @@ exports.getMyGlobalPermissions = async (req, res) => {
         success: true,
         data: req.permissions
     });
-};
-
-exports.fetchUser = async (req, res, next) => {
-    if (req.params.user_id === 'me') {
-        req.currentUser = req.user;
-        return next();
-    }
-
-    // searching the user by url
-    let where = { username: { [Sequelize.Op.iLike]: req.params.user_id } };
-
-    // searching the user by id if it's numeric
-    if (helpers.isNumber(req.params.user_id)) {
-        where = { id: Number(req.params.user_id) };
-    }
-
-    const user = await User.findOne({ where });
-    if (!user) {
-        return errors.makeNotFoundError(res, 'User is not found.');
-    }
-
-    req.currentUser = user;
-
-    // TODO: fetch permissions
-
-    return next();
-};
-
-exports.fetchBody = async (req, res, next) => {
-    // searching the body by code
-    let where = { code: { [Sequelize.Op.iLike]: req.params.body_id } };
-
-    // searching the body by id if it's numeric
-    if (helpers.isNumber(req.params.body_id)) {
-        where = { id: Number(req.params.body_id) };
-    }
-
-    const body = await Body.findOne({ where });
-    if (!body) {
-        return errors.makeNotFoundError(res, 'Body is not found.');
-    }
-
-    req.currentBody = body;
-
-    // TODO: fetch permissions
-
-    return next();
-};
-
-exports.fetchCircle = async (req, res, next) => {
-    // searching the circle by id if it's numeric
-    if (!helpers.isNumber(req.params.circle_id)) {
-        return errors.makeBadRequestError(res, 'Circle ID is invalid.');
-    }
-
-    const circle = await Circle.findOne({
-        where: { id: Number(req.params.circle_id) },
-        include: [
-            { model: Circle, as: 'child_circles' },
-            { model: Circle, as: 'parent_circle' }
-        ]
-    });
-    if (!circle) {
-        return errors.makeNotFoundError(res, 'Circle is not found.');
-    }
-
-    req.currentCircle = circle;
-
-    // TODO: fetch permissions
-
-    return next();
-};
-
-exports.fetchPermission = async (req, res, next) => {
-    // searching the circle by id if it's numeric
-    if (!helpers.isNumber(req.params.permission_id)) {
-        return errors.makeBadRequestError(res, 'Permission ID is invalid.');
-    }
-
-    const permission = await Permission.findOne({
-        where: { id: Number(req.params.permission_id) },
-        include: [
-            { model: Circle, as: 'circles' },
-        ]
-    });
-    if (!permission) {
-        return errors.makeNotFoundError(res, 'Permission is not found.');
-    }
-
-    req.currentPermission = permission;
-
-    // TODO: fetch permissions
-
-    return next();
 };
 
 /* istanbul ignore next */
