3cf7db73650d88e3608519a3886b7e05152aeca4
Sergey Peshkov <serge1peshcoff@gmail.com>
2020-03-02 15:31:08 +0300

    feat(general): auto-adding a person to shadow circle

--- core/models/BodyMembership.js
+++ core/models/BodyMembership.js
@@ -1,4 +1,8 @@
 const { Sequelize, sequelize } = require('../lib/sequelize');
+const logger = require('../lib/logger');
+const Body = require('./Body');
+const Circle = require('./Circle');
+const CircleMembership = require('./CircleMembership');
 
 const BodyMembership = sequelize.define('body_membership', {
     id: {
@@ -22,6 +26,34 @@ const BodyMembership = sequelize.define('body_membership', {
 BodyMembership.beforeValidate(async (membership) => {
     // skipping these fields if they are unset, will catch it later.
     if (typeof membership.comment === 'string') membership.comment = membership.comment.trim();
+});
+
+// Adding a person to a shadow circle after joining the body.
+BodyMembership.afterCreate(async (membership) => {
+    const body = await Body.findByPk(membership.body_id);
+    if (!body.shadow_circle_id) {
+        logger.info('No shadow circle specified for body, not adding person to circle.');
+        return;
+    }
+
+    logger.info(`Adding person ${membership.id} to shadow circle ${body.shadow_circle_id} of body ${body.id}`);
+    await CircleMembership.create({
+        circle_id: body.shadow_circle_id,
+        user_id: membership.user_id
+    });
+});
+
+// Deleting all the circle memberships after leaving the body.
+BodyMembership.afterDestroy(async (membership) => {
+    const circles = await Circle.findAll({ where: { body_id: membership.body_id } });
+    const ids = circles.map((circle) => circle.id);
+
+    await CircleMembership.destroy({
+        where: {
+            user_id: membership.user_id,
+            circle_id: { [Sequelize.Op.in]: ids }
+        }
+    });
 });
 
 module.exports = BodyMembership;
--- core/test/api/memberships-deleting.test.js
+++ core/test/api/memberships-deleting.test.js
@@ -1,7 +1,7 @@
 const { startServer, stopServer } = require('../../lib/server.js');
 const { request } = require('../scripts/helpers');
 const generator = require('../scripts/generator');
-const { BodyMembership } = require('../../models');
+const { BodyMembership, CircleMembership } = require('../../models');
 
 describe('Memberships deleting', () => {
     beforeAll(async () => {
@@ -51,6 +51,30 @@ describe('Memberships deleting', () => {
         expect(res.body).toHaveProperty('message');
 
         const membershipFromDb = await BodyMembership.findByPk(membership.id);
+        expect(membershipFromDb).toEqual(null);
+    });
+
+    test('should remove user from any circles in this body', async () => {
+        const body = await generator.createBody();
+        const circle = await generator.createCircle({ body_id: body.id });
+        const user = await generator.createUser();
+        const token = await generator.createAccessToken({}, user);
+
+        const circleMembership = await generator.createCircleMembership(circle, user);
+        const membership = await generator.createBodyMembership(body, user);
+
+        const res = await request({
+            uri: '/bodies/' + body.id + '/members/' + membership.id,
+            method: 'DELETE',
+            headers: { 'X-Auth-Token': token.value }
+        });
+
+        expect(res.statusCode).toEqual(200);
+        expect(res.body.success).toEqual(true);
+        expect(res.body).not.toHaveProperty('errors');
+        expect(res.body).toHaveProperty('message');
+
+        const membershipFromDb = await CircleMembership.findByPk(circleMembership.id);
         expect(membershipFromDb).toEqual(null);
     });
 });
