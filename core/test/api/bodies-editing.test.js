8473e3e68557f68ea03ca76c9da6e8722fce5d3b
Sergey Peshkov <serge1peshcoff@gmail.com>
2020-03-05 13:00:33 +0300

    feat(general): checking permissions for body

--- core/test/api/bodies-editing.test.js
+++ core/test/api/bodies-editing.test.js
@@ -16,8 +16,11 @@ describe('Bodies editing', () => {
     });
 
     test('should return 404 if the body is not found', async () => {
-        const user = await generator.createUser();
+        const user = await generator.createUser({ superadmin: true });
         const token = await generator.createAccessToken({}, user);
+
+        await generator.createPermission({ scope: 'global', action: 'update', object: 'body' });
+
 
         const res = await request({
             uri: '/bodies/1337',
@@ -33,8 +36,10 @@ describe('Bodies editing', () => {
     });
 
     test('should fail if there are validation errors', async () => {
-        const user = await generator.createUser();
+        const user = await generator.createUser({ superadmin: true });
         const token = await generator.createAccessToken({}, user);
+
+        await generator.createPermission({ scope: 'global', action: 'update', object: 'body' });
 
         const body = await generator.createBody();
 
@@ -52,9 +57,30 @@ describe('Bodies editing', () => {
         expect(res.body.errors).toHaveProperty('email');
     });
 
-    test('should succeed if everything is okay', async () => {
+    test('should fail if no permissions', async () => {
         const user = await generator.createUser();
         const token = await generator.createAccessToken({}, user);
+
+        const body = await generator.createBody();
+
+        const res = await request({
+            uri: '/bodies/' + body.id,
+            method: 'PUT',
+            headers: { 'X-Auth-Token': token.value },
+            body: { email: 'invalid' }
+        });
+
+        expect(res.statusCode).toEqual(403);
+        expect(res.body.success).toEqual(false);
+        expect(res.body).not.toHaveProperty('data');
+        expect(res.body).toHaveProperty('message');
+    });
+
+    test('should succeed on global permission', async () => {
+        const user = await generator.createUser({ superadmin: true });
+        const token = await generator.createAccessToken({}, user);
+
+        await generator.createPermission({ scope: 'global', action: 'update', object: 'body' });
 
         const body = await generator.createBody();
 
@@ -70,5 +96,67 @@ describe('Bodies editing', () => {
         expect(res.body).not.toHaveProperty('errors');
         expect(res.body).toHaveProperty('data');
         expect(res.body.data.email).toEqual('test@test.io');
+    });
+
+    test('should succeed on local permission', async () => {
+        const user = await generator.createUser({ superadmin: true });
+        const token = await generator.createAccessToken({}, user);
+
+        const permission = await generator.createPermission({
+            scope: 'local',
+            action: 'update',
+            object: 'body'
+        });
+
+        const body = await generator.createBody();
+        const circle = await generator.createCircle({ body_id: body.id });
+        await generator.createBodyMembership(body, user);
+        await generator.createCircleMembership(circle, user);
+        await generator.createCirclePermission(circle, permission);
+
+        const res = await request({
+            uri: '/bodies/' + body.id,
+            method: 'PUT',
+            headers: { 'X-Auth-Token': token.value },
+            body: { email: 'test@test.io' }
+        });
+
+        expect(res.statusCode).toEqual(200);
+        expect(res.body.success).toEqual(true);
+        expect(res.body).not.toHaveProperty('errors');
+        expect(res.body).toHaveProperty('data');
+        expect(res.body.data.email).toEqual('test@test.io');
+    });
+
+    test('should respect filters', async () => {
+        const user = await generator.createUser({ superadmin: true });
+        const token = await generator.createAccessToken({}, user);
+
+        const permission = await generator.createPermission({
+            scope: 'local',
+            action: 'update',
+            object: 'body',
+            filters: ['name']
+        });
+
+        const body = await generator.createBody({ name: 'aaa' });
+        const circle = await generator.createCircle({ body_id: body.id });
+        await generator.createBodyMembership(body, user);
+        await generator.createCircleMembership(circle, user);
+        await generator.createCirclePermission(circle, permission);
+
+        const res = await request({
+            uri: '/bodies/' + body.id,
+            method: 'PUT',
+            headers: { 'X-Auth-Token': token.value },
+            body: { email: 'test@test.io', name: 'bbb' }
+        });
+
+        expect(res.statusCode).toEqual(200);
+        expect(res.body.success).toEqual(true);
+        expect(res.body).not.toHaveProperty('errors');
+        expect(res.body).toHaveProperty('data');
+        expect(res.body.data.email).not.toEqual('test@test.io');
+        expect(res.body.data.name).toEqual('bbb');
     });
 });
--- core/test/api/bodies-status.test.js
+++ core/test/api/bodies-status.test.js
@@ -16,8 +16,10 @@ describe('Bodies status', () => {
     });
 
     test('should return 404 if the body is not found', async () => {
-        const user = await generator.createUser();
+        const user = await generator.createUser({ superadmin: true });
         const token = await generator.createAccessToken({}, user);
+
+        await generator.createPermission({ scope: 'global', action: 'delete', object: 'body' });
 
         const res = await request({
             uri: '/bodies/1337/status',
@@ -33,8 +35,10 @@ describe('Bodies status', () => {
     });
 
     test('should fail if there are validation errors', async () => {
-        const user = await generator.createUser();
+        const user = await generator.createUser({ superadmin: true });
         const token = await generator.createAccessToken({}, user);
+
+        await generator.createPermission({ scope: 'global', action: 'delete', object: 'body' });
 
         const body = await generator.createBody();
 
@@ -52,9 +56,30 @@ describe('Bodies status', () => {
         expect(res.body.errors).toHaveProperty('status');
     });
 
-    test('should succeed if everything is okay', async () => {
+    test('should fail if no permissions', async () => {
         const user = await generator.createUser();
         const token = await generator.createAccessToken({}, user);
+
+        const body = await generator.createBody();
+
+        const res = await request({
+            uri: '/bodies/' + body.id + '/status',
+            method: 'PUT',
+            headers: { 'X-Auth-Token': token.value },
+            body: { status: 'aaa' }
+        });
+
+        expect(res.statusCode).toEqual(403);
+        expect(res.body.success).toEqual(false);
+        expect(res.body).not.toHaveProperty('data');
+        expect(res.body).toHaveProperty('message');
+    });
+
+    test('should succeed if everything is okay', async () => {
+        const user = await generator.createUser({ superadmin: true });
+        const token = await generator.createAccessToken({}, user);
+
+        await generator.createPermission({ scope: 'global', action: 'delete', object: 'body' });
 
         const body = await generator.createBody();
 
