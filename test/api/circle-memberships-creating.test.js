921368b9563e7da88983574314d9b2aa1093a602
Sergey Peshkov <serge1peshcoff@gmail.com>
2020-03-11 21:53:48 +0300

    feat(general): permissions checking for circles

--- test/api/circle-memberships-creating.test.js
+++ test/api/circle-memberships-creating.test.js
@@ -16,8 +16,10 @@ describe('Circle memberships creating', () => {
     });
 
     test('should return 400 if the user_id is invalid', async () => {
-        const currentUser = await generator.createUser({ username: 'test', mail_confirmed_at: new Date() });
+        const currentUser = await generator.createUser({ superadmin: true });
         const token = await generator.createAccessToken({}, currentUser);
+
+        await generator.createPermission({ scope: 'global', action: 'add_member', object: 'circle' });
 
         const circle = await generator.createCircle();
 
@@ -35,8 +37,10 @@ describe('Circle memberships creating', () => {
     });
 
     test('should return 404 if the user is not found', async () => {
-        const currentUser = await generator.createUser({ username: 'test', mail_confirmed_at: new Date() });
+        const currentUser = await generator.createUser({ superadmin: true });
         const token = await generator.createAccessToken({}, currentUser);
+
+        await generator.createPermission({ scope: 'global', action: 'add_member', object: 'circle' });
 
         const circle = await generator.createCircle();
 
@@ -54,8 +58,10 @@ describe('Circle memberships creating', () => {
     });
 
     test('should fail if the user is not a member of a body', async () => {
-        const currentUser = await generator.createUser({ username: 'test', mail_confirmed_at: new Date() });
+        const currentUser = await generator.createUser({ superadmin: true });
         const token = await generator.createAccessToken({}, currentUser);
+
+        await generator.createPermission({ scope: 'global', action: 'add_member', object: 'circle' });
 
         const body = await generator.createBody();
         const circle = await generator.createCircle({ body_id: body.id });
@@ -75,8 +81,10 @@ describe('Circle memberships creating', () => {
     });
 
     test('should succeed if everything is okay for a bound circle', async () => {
-        const currentUser = await generator.createUser({ username: 'test', mail_confirmed_at: new Date() });
+        const currentUser = await generator.createUser({ superadmin: true });
         const token = await generator.createAccessToken({}, currentUser);
+
+        await generator.createPermission({ scope: 'global', action: 'add_member', object: 'circle' });
 
         const body = await generator.createBody();
         const circle = await generator.createCircle({ body_id: body.id });
@@ -96,12 +104,63 @@ describe('Circle memberships creating', () => {
         expect(res.body).toHaveProperty('data');
     });
 
-    test('should succeed if everything is okay for a free circle', async () => {
-        const currentUser = await generator.createUser({ username: 'test', mail_confirmed_at: new Date() });
+    test('should fail if no permission', async () => {
+        const currentUser = await generator.createUser();
         const token = await generator.createAccessToken({}, currentUser);
 
         const circle = await generator.createCircle();
         const user = await generator.createUser();
+
+        const res = await request({
+            uri: '/circles/' + circle.id + '/members/' + user.id,
+            method: 'POST',
+            headers: { 'X-Auth-Token': token.value },
+            body: circle
+        });
+
+        expect(res.statusCode).toEqual(403);
+        expect(res.body.success).toEqual(false);
+        expect(res.body).not.toHaveProperty('data');
+        expect(res.body).toHaveProperty('message');
+    });
+
+    test('should succeed if everything is okay for a free circle', async () => {
+        const currentUser = await generator.createUser({ superadmin: true });
+        const token = await generator.createAccessToken({}, currentUser);
+
+        await generator.createPermission({ scope: 'global', action: 'add_member', object: 'circle' });
+
+        const circle = await generator.createCircle();
+        const user = await generator.createUser();
+
+        const res = await request({
+            uri: '/circles/' + circle.id + '/members/' + user.id,
+            method: 'POST',
+            headers: { 'X-Auth-Token': token.value },
+            body: circle
+        });
+
+        expect(res.statusCode).toEqual(200);
+        expect(res.body.success).toEqual(true);
+        expect(res.body).not.toHaveProperty('errors');
+        expect(res.body).toHaveProperty('data');
+    });
+
+    test('should work with local permission', async () => {
+        const currentUser = await generator.createUser();
+        const token = await generator.createAccessToken({}, currentUser);
+
+        const permission = await generator.createPermission({ scope: 'local', action: 'add_member', object: 'circle' });
+
+        const body = await generator.createBody();
+        const permissionCircle = await generator.createCircle({ body_id: body.id });
+        await generator.createBodyMembership(body, currentUser);
+        await generator.createCircleMembership(permissionCircle, currentUser);
+        await generator.createCirclePermission(permissionCircle, permission);
+
+        const circle = await generator.createCircle({ body_id: body.id });
+        const user = await generator.createUser();
+        await generator.createBodyMembership(body, user);
 
         const res = await request({
             uri: '/circles/' + circle.id + '/members/' + user.id,
