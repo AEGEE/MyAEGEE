c1eab5f9ad43aead83b6f67da7ee43f79339a818
Sergey Peshkov <serge1peshcoff@gmail.com>
2020-03-12 21:40:35 +0300

    feat(general): checking permissions for join requests

--- core/test/api/join-requests-listing.test.js
+++ core/test/api/join-requests-listing.test.js
@@ -16,11 +16,13 @@ describe('Join requests list', () => {
     });
 
     test('should succeed when everything is okay', async () => {
-        const user = await generator.createUser({ password: 'test', mail_confirmed_at: new Date() });
+        const user = await generator.createUser({ superadmin: true });
         const token = await generator.createAccessToken({}, user);
 
         const body = await generator.createBody();
         const joinRequest = await generator.createJoinRequest(body, user);
+
+        await generator.createPermission({ scope: 'global', action: 'view', object: 'join_request' });
 
         const res = await request({
             uri: '/bodies/' + body.id + '/join-requests',
@@ -38,9 +40,11 @@ describe('Join requests list', () => {
     });
 
     test('should respect limit and offset', async () => {
-        const user = await generator.createUser({ password: 'test', mail_confirmed_at: new Date() });
+        const user = await generator.createUser({ superadmin: true });
         const token = await generator.createAccessToken({}, user);
         const body = await generator.createBody();
+
+        await generator.createPermission({ scope: 'global', action: 'view', object: 'join_request' });
 
         const firstUser = await generator.createUser();
         await generator.createJoinRequest(body, firstUser);
@@ -69,7 +73,7 @@ describe('Join requests list', () => {
     });
 
     test('should respect sorting', async () => {
-        const user = await generator.createUser({ password: 'test', mail_confirmed_at: new Date() });
+        const user = await generator.createUser({ superadmin: true });
         const token = await generator.createAccessToken({}, user);
         const body = await generator.createBody();
 
@@ -78,6 +82,8 @@ describe('Join requests list', () => {
 
         const secondUser = await generator.createUser();
         const secondJoinRequest = await generator.createJoinRequest(body, secondUser);
+
+        await generator.createPermission({ scope: 'global', action: 'view', object: 'join_request' });
 
         const res = await request({
             uri: '/bodies/' + body.id + '/join-requests?sort=id&direction=desc', // second one should be returned
@@ -94,5 +100,50 @@ describe('Join requests list', () => {
         expect(res.body.data.length).toEqual(2);
         expect(res.body.data[0].id).toEqual(secondJoinRequest.id);
         expect(res.body.data[1].id).toEqual(firstJoinRequest.id);
+    });
+
+    test('should work with local permission', async () => {
+        const user = await generator.createUser();
+        const token = await generator.createAccessToken({}, user);
+
+        const body = await generator.createBody();
+        const joinRequest = await generator.createJoinRequest(body, user);
+
+        const permission = await generator.createPermission({ scope: 'local', action: 'view', object: 'join_request' });
+        const circle = await generator.createCircle({ body_id: body.id });
+        await generator.createCirclePermission(circle, permission);
+        await generator.createCircleMembership(circle, user);
+
+        const res = await request({
+            uri: '/bodies/' + body.id + '/join-requests',
+            method: 'GET',
+            headers: { 'X-Auth-Token': token.value }
+        });
+
+        expect(res.statusCode).toEqual(200);
+        expect(res.body.success).toEqual(true);
+        expect(res.body).toHaveProperty('data');
+        expect(res.body).not.toHaveProperty('errors');
+
+        expect(res.body.data.length).toEqual(1);
+        expect(res.body.data[0].id).toEqual(joinRequest.id);
+    });
+
+    test('should succeed when everything is okay', async () => {
+        const user = await generator.createUser();
+        const token = await generator.createAccessToken({}, user);
+
+        const body = await generator.createBody();
+
+        const res = await request({
+            uri: '/bodies/' + body.id + '/join-requests',
+            method: 'GET',
+            headers: { 'X-Auth-Token': token.value }
+        });
+
+        expect(res.statusCode).toEqual(403);
+        expect(res.body.success).toEqual(false);
+        expect(res.body).toHaveProperty('message');
+        expect(res.body).not.toHaveProperty('data');
     });
 });
