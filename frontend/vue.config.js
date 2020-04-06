249c8df3d9bb8212f5ce4c3c50ca5fc8f313d449
Дилян Палаузов <dpa-github@aegee.org>
2020-04-06 18:42:13 +0000

    feat: compress the output using gz/zopfli and brotli (#282)
    * feat: compress gz/zopfli the output
    
    * homework: enable in nginx “gzip_static on;” so that the .gz files are
      delivered to the users.
    
    * feat: compress the output using brotli
    
    * homework: ensure that Node JS is at least 11.7 or install dependency iltorb
    * homework: ensure that nginx has the ngx_brotli module,
        see https://github.com/google/ngx_brotli
    * homework: enable in nginx “brotli_static on;” so that the .br files are
        delivered to the users.
    
    Co-authored-by: Дилян Палаузов <git-dpa@aegee.org>

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -18,3 +18,35 @@ module.exports = {
     }
   }
 }
+
+if (process.env.NODE_ENV === 'production') {
+  const CompressionPlugin = require('compression-webpack-plugin');
+
+  module.exports.configureWebpack.plugins = [
+    new CompressionPlugin({
+      algorithm: require("@gfx/zopfli").gzip,
+      compressionOptions: {
+        numiterations: 15
+      },
+      minRatio: 0.99,
+      test:  /\.(js|css|json|html|ico|svg)(\?.*)?$/i
+    })
+  ];
+  if (require('zlib').brotliCompress) // Since Node 11.7
+    module.exports.configureWebpack.plugins.push(
+      new CompressionPlugin({
+        filename: '[path].br[query]',
+        algorithm: 'brotliCompress',
+        compressionOptions: { level:11 }, //matches BROTLI_MAX_QUALITY
+        minRatio: 0.99,
+        test:  /\.(js|css|json|html|ico|svg)(\?.*)?$/i
+    }));
+  else //install optionalDependency iltorb
+    module.exports.configureWebpack.plugins.push(
+      new CompressionPlugin({
+        filename: '[path].br[query]',
+        algorithm: require('iltorb').compress,
+        minRatio: 0.99,
+        test:  /\.(js|css|json|html|ico|svg)(\?.*)?$/i
+    }));
+}
