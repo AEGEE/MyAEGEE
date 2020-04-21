c8801ada3c6c313eb1183b14899f79360d6bfa2e
Sergey Peshkov <serge1peshcoff@gmail.com>
2020-04-21 23:56:24 +0300

    feat(circles): circles memberships crud + refactor

--- models/CircleMembership.js
+++ models/CircleMembership.js
@@ -1,6 +1,12 @@
 const { Sequelize, sequelize } = require('../lib/sequelize');
 
 const CircleMembership = sequelize.define('circle_membership', {
+    id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+    },
     position: {
         type: Sequelize.TEXT,
         allowNull: true
