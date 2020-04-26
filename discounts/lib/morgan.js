1005f3aa0bf0489637f507968464f966399b36e2
Sergey Peshkov <serge1peshcoff@users.noreply.github.com>
2020-04-26 13:14:03 +0300

    feat(logging): switch to bunyan, log as json (#47)
    * feat(logging): switch to bunyan, log as json
    
    * chore(ci): do not build on master

--- discounts/lib/morgan.js
+++ discounts/lib/morgan.js
@@ -3,18 +3,13 @@ const morgan = require('morgan');
 const log = require('./logger');
 
 module.exports = morgan((tokens, req, res) => {
-    let result = [
-        tokens.method(req, res),
-        tokens.url(req, res),
-        tokens.status(req, res),
-        tokens.res(req, res, 'content-length'), '-',
-        tokens['response-time'](req, res), 'ms,',
-        req.user ? ('user ' + req.user.user.name + ' with id ' + req.user.id) : 'unauthorized'
-    ].join(' ');
-
-    if (['PUT', 'POST'].includes(tokens.method(req, res))) {
-        result += ', request body: ' + JSON.stringify(req.body, null, '  ');
-    }
-
-    return result;
-}, { stream: log.stream });
+    log.info({
+        method: tokens.method(req, res),
+        url: tokens.url(req, res),
+        status: tokens.status(req, res),
+        length: tokens.res(req, res, 'content-length'),
+        'response-time': tokens['response-time'](req, res),
+        user: req.user,
+        body: req.body
+    }, 'Request processed');
+});
--- discounts/lib/server.js
+++ discounts/lib/server.js
@@ -21,7 +21,7 @@ const GeneralRouter = router({ mergeParams: true });
 
 /* istanbul ignore next */
 process.on('unhandledRejection', (err) => {
-    log.error('Unhandled rejection: %s', err.stack);
+    log.error({ err }, 'Unhandled rejection');
 
     if (process.env.NODE_ENV !== 'test') {
         bugsnag.notify(err);
@@ -59,10 +59,10 @@ server.use(middlewares.errorHandler);
 let app;
 async function startServer() {
     return new Promise((res, rej) => {
-        log.info('Starting server with the following config: %o', config);
+        log.info({ config }, 'Starting server with the following config');
         const localApp = server.listen(config.port, async () => {
             app = localApp;
-            log.info('Up and running, listening on http://localhost:%d', config.port);
+            log.info({ host: 'http://localhost:' + config.port }, 'Up and running, listening');
             await db.authenticate();
             return res();
         });
