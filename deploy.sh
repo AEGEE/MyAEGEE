ff10e204759dc344e249e3473db68bfed4519e44
Sergey Peshkov <serge1peshcoff@gmail.com>
2020-05-08 23:12:02 +0300

    feat(deploy): added deploy script

---
+++ deploy.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+export $(grep -v '^#' ".env" | gxargs -d '\n')
+
+if [[ "$#" -lt 1 ]]; then
+    echo "Usage: ./deploy.sh <service-name>"
+    exit 1
+fi
+
+SERVICE_NAME=$1
+
+echo "Pulling $SERVICE_NAME..."
+ssh -p "$DEPLOY_PORT" "$DEPLOY_USER@$DEPLOY_HOST" "cd $DEPLOY_FOLDER && ./helper.sh --pull $SERVICE_NAME"
+
+echo "(Re)starting $SERVICE_NAME..."
+ssh -p "$DEPLOY_PORT" "$DEPLOY_USER@$DEPLOY_HOST" "cd $DEPLOY_FOLDER && ./helper.sh --start $SERVICE_NAME"
+
+echo "Sending a Slack notice..."
+curl $DEPLOY_TOKEN -X POST -H 'Content-Type: application/json' --data-binary @- << EOF
+{
+    "attachments": [
+        {
+            "fallback": "The Docker image for \`$SERVICE_NAME\` was deployed to \`$DEPLOY_HOST\`.",
+            "color": "#36a64f",
+            "pretext": "The Docker image for \`$SERVICE_NAME\` was deployed to \`$DEPLOY_HOST\`.",
+            "fields": [
+                {
+                    "title": "Service",
+                    "value": "$SERVICE_NAME",
+                    "short": false
+                }
+            ],
+            "image_url": "http://my-website.com/path/to/image.jpg",
+            "thumb_url": "http://example.com/path/to/thumb.png",
+            "ts": $(date +%s)
+        }
+    ]
+}
+EOF
