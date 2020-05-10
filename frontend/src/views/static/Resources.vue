016ee68a6e3d1f22ab0399dd97605575cb4f20b1
Rik Smale <WikiRik@users.noreply.github.com>
2020-05-10 21:19:18 +0200

    fix(resources): changed colours to light and removed images. Fixes HELP-512 (#353)
    * fix(resources): changed colours to light shade and removed images. Fixes HELP-512
    
    * chore: fix lint

--- frontend/src/views/static/Resources.vue
+++ frontend/src/views/static/Resources.vue
@@ -67,29 +67,25 @@ export default {
             url: 'http://aegee.org',
             title: 'AEGEE website',
             description: 'Official website',
-            img: 'default-blue.png',
-            color: '#1468C5'
+            color: '#4CA0FF'
           },
           {
             url: 'http://aegee.blogactiv.eu/',
             title: 'External blog',
             description: 'Blog hosted on Blogactiv platform',
-            img: 'default-blue.png',
-            color: '#1468C5'
+            color: '#4CA0FF'
           },
           {
             url: 'http://aegee.us7.list-manage.com/subscribe?u=7a170c64e0d3990f3f6629d21&id=fc8908d780',
             title: 'External newsletter',
             description: 'Bi-monthly newsletter for Partners & Supporters',
-            img: 'default-blue.png',
-            color: '#1468C5'
+            color: '#4CA0FF'
           },
           {
             url: 'https://www.aegee.org/external/key-to-europe/',
             title: 'Key to Europe',
             description: 'Yearbook - the annual report publication',
-            img: 'none.png',
-            color: '#1468C5'
+            color: '#4CA0FF'
           }
         ],
         Members: [
@@ -97,57 +93,49 @@ export default {
             url: 'http://www.zeus.aegee.org/portal/',
             title: 'Internal news',
             description: 'Open calls and news from European Bodies',
-            img: 'members-portal.png',
-            color: '#FBBA00'
+            color: '#FFDB4C'
           },
           {
             url: 'https://lists.aegee.org/join/AEGEENEWS-L',
             title: 'Internal newsletter',
             description: 'Bi-weekly internal newsletter',
-            img: 'default-yellow.png',
-            color: '#FBBA00'
+            color: '#FFDB4C'
           },
           {
             url: 'http://www.zeus.aegee.org/magazine/',
             title: 'The AEGEEan magazine',
             description: 'AEGEE\'s online Magazine',
-            img: 'none.png',
-            color: '#FBBA00'
+            color: '#FFDB4C'
           },
           {
             url: 'http://aegee.org/wiki',
             title: 'AEGEE Wiki',
             description: 'A collaborative knowledge backbone of AEGEE',
-            img: 'wiki.png',
-            color: '#FBBA00'
+            color: '#FFDB4C'
           },
           {
             url: 'https://www.zeus.aegee.org/forum',
             title: 'Forum',
             description: 'Place for structured discussions ',
-            img: 'default-purple.png',
-            color: '#FBBA00'
+            color: '#FFDB4C'
           },
           {
             url: 'https://lists.aegee.org/?HOME',
             title: 'Mailing lists',
             description: 'AEGEE mailing lists',
-            img: 'default-purple.png',
-            color: '#FBBA00'
+            color: '#FFDB4C'
           },
           {
             url: 'https://www.facebook.com/groups/aegeeans/',
             title: 'Facebook group',
             description: 'AEGEE\'s official group on Facebook',
-            img: 'facebook.png',
-            color: '#FBBA00'
+            color: '#FFDB4C'
           },
           {
             url: 'https://site.aegee.org/wp-content/uploads/AEGEE-StrategicPlan-2020-2023.pdf',
             title: 'Strategic Plan',
             description: 'Strategic Plan of AEGEE-Europe 2020-2023',
-            img: 'facebook.png',
-            color: '#FBBA00'
+            color: '#FFDB4C'
           }
         ],
         Statutory: [
@@ -155,15 +143,13 @@ export default {
             url: 'https://www.zeus.aegee.org/statutoryvote/jc/',
             title: 'Online voting',
             description: 'Online Voting for Agorae/General Assembly',
-            img: 'none.png',
-            color: '#A0C514'
+            color: '#C44BC2'
           },
           {
             url: 'https://www.zeus.aegee.org/portal/cia-v-30-1/',
             title: 'CIA',
             description: 'Statutes, regulation, working formats and motions',
-            img: 'none.png',
-            color: '#A0C514'
+            color: '#C44BC2'
           }
         ],
         'Useful guides': [
@@ -171,22 +157,19 @@ export default {
             url: 'http://www.zeus.aegee.org/portal/resources/pr-materials/',
             title: 'Visual identity',
             description: 'Manual, logos, PR materials and more',
-            img: 'default-red.png',
-            color: '#C51C13'
+            color: '#FF5543'
           },
           {
             url: 'https://www.zeus.aegee.org/portal/resources/internal-education/',
             title: 'Internal education',
             description: 'LTC and RTC packages.',
-            img: 'default-red.png',
-            color: '#C51C13'
+            color: '#FF5543'
           },
           {
             url: 'https://www.zeus.aegee.org/portal/resources/members-manual/',
             title: 'Members manual',
             description: 'Members Manual, guides etc.',
-            img: 'default-red.png',
-            color: '#C51C13'
+            color: '#FF5543'
           }
         ]
       }
