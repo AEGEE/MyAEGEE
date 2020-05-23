cc782c9c1a2897c7b7df468fbcc2e8e5b10d1c1f
Sergey Peshkov <serge1peshcoff@gmail.com>
2020-05-23 20:49:19 +0300

    fix(bodies): add description

--- migrations/20200201191802-create-bodies.js
+++ migrations/20200201191802-create-bodies.js
@@ -10,6 +10,10 @@ module.exports = {
             type: Sequelize.STRING,
             allowNull: false
         },
+        description: {
+            type: Sequelize.TEXT,
+            allowNull: false
+        },
         code: {
             type: Sequelize.STRING,
             allowNull: false,
