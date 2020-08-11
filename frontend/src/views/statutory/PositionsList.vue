2ffcbe08c831f2dbb09c058dc19aa999d9de6884
LeonVreling <49942406+LeonVreling@users.noreply.github.com>
2020-08-11 16:07:30 +0200

    fix: different weird things with empty task descriptions (#518)
    * fix: different weird things with empty task descriptions
    
    * chore: javascript is annoying

--- frontend/src/views/statutory/PositionsList.vue
+++ frontend/src/views/statutory/PositionsList.vue
@@ -200,7 +200,7 @@ export default {
       return this.$route.params.prefix
     },
     taskDescription () {
-      if (!this.selectedPosition.body_id) {
+      if (!this.selectedPosition.body_id || !this.selectedPosition.task_description || this.selectedPosition.task_description === '') {
         return 'A description for this position has not been set.'
       }
       const body = this.bodies.find(bod => bod.id === this.selectedPosition.body_id)
