7022fb4451b69529537ffe1f8aab34cb1d5e59d8
Rik Smale <WikiRik@users.noreply.github.com>
2020-11-24 11:05:01 +0100

    chore(dos2unix): dos2unix all sh files (#218)
    Not the nicest solution, but it works

--- vagrant-post-script/orchestrate_docker.sh
+++ vagrant-post-script/orchestrate_docker.sh
@@ -2,13 +2,16 @@
 
 cd /vagrant/ || exit 1
 
+# find function copied from https://stackoverflow.com/questions/47712039/how-to-run-dos2unix-for-all-the-files-in-subfolders-in-bash
 if [[ -f .env ]]; then
   chmod +x helper.sh
-  dos2unix .env Makefile helper.sh
+  find . -type f -name "*.sh" -exec dos2unix {} \+;
+  dos2unix .env Makefile
   make start
 else
   cp .env.example .env
   chmod +x helper.sh
-  dos2unix .env Makefile helper.sh
+  find . -type f -name "*.sh" -exec dos2unix {} \+;
+  dos2unix .env Makefile
   make bootstrap
 fi
