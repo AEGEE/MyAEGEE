0eb6b10bbf4f05f0c96be5b100d87f0c7b2d0e6c
LeonVreling <49942406+LeonVreling@users.noreply.github.com>
2021-02-08 14:50:56 +0100

    fix(mail): update to field mailer (#438)
    Co-authored-by: Rik Smale <wikirik000@gmail.com>
    Co-authored-by: Rik Smale <WikiRik@users.noreply.github.com>

--- statutory/lib/applications.js
+++ statutory/lib/applications.js
@@ -255,7 +255,7 @@ exports.updateApplication = async (req, res) => {
     req.body.first_name = user.first_name;
     req.body.last_name = user.last_name;
     req.body.gender = user.gender;
-    req.body.email = user.email;
+    req.body.email = user.notification_email;
     req.body.date_of_birth = user.date_of_birth;
     if (req.body.body_id) {
         // Shouldn't crash, if the person is not a member of a body,
@@ -296,7 +296,7 @@ exports.updateApplication = async (req, res) => {
 
         if (boardMembers.length > 0) {
             await mailer.sendMail({
-                to: boardMembers.map((member) => member.user.email),
+                to: boardMembers.map((member) => member.user.notification_email),
                 subject: `One of your body members changed the application to ${req.event.name}`,
                 template: 'statutory_board_edited.html',
                 parameters: {
@@ -612,7 +612,7 @@ exports.postApplication = async (req, res) => {
     req.body.first_name = req.user.first_name;
     req.body.last_name = req.user.last_name;
     req.body.gender = req.user.gender;
-    req.body.email = req.user.email;
+    req.body.email = req.user.notification_email;
     req.body.body_name = req.user.bodies.find((b) => req.body.body_id === b.id).name;
     req.body.date_of_birth = req.user.date_of_birth;
 
@@ -643,7 +643,7 @@ exports.postApplication = async (req, res) => {
 
         if (boardMembers.length > 0) {
             await mailer.sendMail({
-                to: boardMembers.map((member) => member.user.email),
+                to: boardMembers.map((member) => member.user.notification_email),
                 subject: `One of your body members has applied to ${req.event.name}`,
                 template: 'statutory_board_applied.html',
                 parameters: {
