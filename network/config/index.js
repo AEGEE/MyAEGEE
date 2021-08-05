7086c9f635f19857d56c2b2137e6cffc63aeab7c
Rik Smale <WikiRik@users.noreply.github.com>
2021-08-05 13:48:59 +0200

    fix(logging): log info and above by default (#143)

--- network/config/index.js
+++ network/config/index.js
@@ -24,7 +24,7 @@ const config = {
         },
         logger: {
             silent: false,
-            level: process.env.LOGLEVEL || 'debug'
+            level: process.env.LOGLEVEL || 'info'
         },
         host: process.env.HOST || 'localhost',
         bugsnagKey: process.env.BUGSNAG_KEY || ''
