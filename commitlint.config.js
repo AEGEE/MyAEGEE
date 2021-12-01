460987d018f3c999a29f22085b162de3dac717cb
Rik Smale <13023439+WikiRik@users.noreply.github.com>
2021-12-01 20:29:08 +0100

    chore(commitlint): fix commitlint (#401)

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,6 +1,7 @@
 module.exports = {
     extends: ['@commitlint/config-conventional'],
     rules: {
-        'header-max-length': [0]
+        'header-max-length': [0],
+        'footer-max-line-length': [0],
     }
 };
