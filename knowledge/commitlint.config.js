7c40533936ae5c8de32a333a4061857ef337a628
Rik Smale <13023439+WikiRik@users.noreply.github.com>
2021-12-01 18:30:43 +0100

    fix(deps): update to Node 16 and upgrade deps and CI config (#131)
    * fix(deps): update to Node 16 and upgrade deps and CI config
    
    * chore(docker-compose): revert docker-compose version

--- knowledge/commitlint.config.js
+++ knowledge/commitlint.config.js
@@ -1,6 +1,6 @@
 module.exports = {
     extends: ['@commitlint/config-conventional'],
     rules: {
-        'footer-max-line-length': [0, 'always', 100],
+        'footer-max-line-length': [0],
     }
 };
