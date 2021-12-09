db02f6c8fe778ebc61ca985e234410580dca8ce3
Rik Smale <13023439+WikiRik@users.noreply.github.com>
2021-12-09 15:58:04 +0100

    chore(superadmin): fix tests (#402)

--- core/test/api/users-superadmin.test.js
+++ core/test/api/users-superadmin.test.js
@@ -17,7 +17,7 @@ describe('User superadmin', () => {
 
     test('should return 404 if the user is not found', async () => {
         const user = await generator.createUser({ superadmin: true });
-        const token = await generator.createAccessToken({}, user);
+        const token = await generator.createAccessToken(user);
 
         await generator.createPermission({ scope: 'global', action: 'update_superadmin', object: 'member' });
 
@@ -36,7 +36,7 @@ describe('User superadmin', () => {
 
     test('should fail if there are validation errors', async () => {
         const user = await generator.createUser({ superadmin: true });
-        const token = await generator.createAccessToken({}, user);
+        const token = await generator.createAccessToken(user);
 
         await generator.createPermission({ scope: 'global', action: 'update_superadmin', object: 'member' });
 
@@ -56,7 +56,7 @@ describe('User superadmin', () => {
 
     test('should succeed if everything is okay', async () => {
         const user = await generator.createUser({ superadmin: true });
-        const token = await generator.createAccessToken({}, user);
+        const token = await generator.createAccessToken(user);
 
         await generator.createPermission({ scope: 'global', action: 'update_superadmin', object: 'member' });
 
@@ -76,7 +76,7 @@ describe('User superadmin', () => {
 
     test('should fail for yourself without permission', async () => {
         const user = await generator.createUser();
-        const token = await generator.createAccessToken({}, user);
+        const token = await generator.createAccessToken(user);
 
         const res = await request({
             uri: '/members/' + user.id + '/superadmin',
@@ -93,7 +93,7 @@ describe('User superadmin', () => {
 
     test('should not work with local permission', async () => {
         const user = await generator.createUser();
-        const token = await generator.createAccessToken({}, user);
+        const token = await generator.createAccessToken(user);
 
         const otherUser = await generator.createUser();
         const permission = await generator.createPermission({ scope: 'local', action: 'update_superadmin', object: 'member' });
@@ -118,7 +118,7 @@ describe('User superadmin', () => {
 
     test('should fail for other person without permission', async () => {
         const user = await generator.createUser();
-        const token = await generator.createAccessToken({}, user);
+        const token = await generator.createAccessToken(user);
 
         const otherUser = await generator.createUser();
 
