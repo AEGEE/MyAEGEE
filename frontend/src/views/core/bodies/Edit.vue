e377a47c1f8bc62bb5a4568e7045d58005aa589e
Olta Dedej <35349342+oltadedej@users.noreply.github.com>
2021-12-25 13:00:49 +0100

    fix(body): validate body code to be 3 letters (#1254)
    * fix(body): validate body code to be 3 letters
    
    * fix(body): implement requested changes
    
    Co-authored-by: Rik Smale <13023439+WikiRik@users.noreply.github.com>

--- frontend/src/views/core/bodies/Edit.vue
+++ frontend/src/views/core/bodies/Edit.vue
@@ -69,7 +69,7 @@
         <div class="field">
           <label class="label">Body code <span class="has-text-danger">*</span></label>
           <div class="control">
-            <input class="input" type="text" required :disabled="!can.editCode" v-model="body.code" />
+            <input class="input" type="text" required :disabled="!can.editCode" v-model="body.code" pattern="[A-Za-z]{3}" title="Body code should contain only 3 letters"/>
           </div>
           <p class="help is-danger" v-if="errors.code">{{ errors.code.join(', ')}}</p>
         </div>
