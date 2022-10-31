5010cb3c0261373c5cfcefb8bd7ae787eab2a321
linuxbandit <bellicaf@tcd.ie>
2022-10-31 17:23:57 +0200

    feat: add bootstrap files for python

---
+++ vagrant-post-script/setup_python.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# call it with
+#   ./setup_python.sh venv
+# (or just any argument) will have it installed for local dev
+#   ./setup_python.sh
+# will have it install the dependencies globally
+# NB: The server WILL NEED globally installed
+
+SCRIPT_DIR=$(dirname "${0}")
+VENV=${1}
+
+if [[ -z ${VENV} ]]; then # globally deps
+  pip install -r "${SCRIPT_DIR}/requirements.txt"
+else
+  python3 -m venv myaegee-venv
+  source ./myaegee-venv/bin/activate
+  pip install -r "${SCRIPT_DIR}/requirements.txt"
+  deactivate
+fi
