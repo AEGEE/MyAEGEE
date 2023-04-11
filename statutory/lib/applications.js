688184d2869dea6c6da2bbba6996749f8bbd2bf4
Rik Smale <13023439+WikiRik@users.noreply.github.com>
2023-04-11 10:10:04 +0200

    fix(appplications): add waiting list to stats (#986)
    See https://github.com/AEGEE/frontend/pull/1697

--- statutory/lib/applications.js
+++ statutory/lib/applications.js
@@ -127,6 +127,7 @@ exports.getStats = async (req, res) => {
         accepted: applications.filter((app) => helpers.filterObject(app, { cancelled: false, status: 'accepted' })).length,
         rejected: applications.filter((app) => helpers.filterObject(app, { cancelled: false, status: 'rejected' })).length,
         pending: applications.filter((app) => helpers.filterObject(app, { cancelled: false, status: 'pending' })).length,
+        waiting_list: applications.filter((app) => helpers.filterObject(app, { cancelled: false, status: 'waiting_list' })).length,
         cancelled: applications.filter((app) => helpers.filterObject(app, { cancelled: true })).length,
         confirmed: applications.filter((app) => helpers.filterObject(app, { confirmed: true })).length,
         registered: applications.filter((app) => helpers.filterObject(app, { registered: true })).length,
