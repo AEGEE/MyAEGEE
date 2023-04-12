5b4820decba7ba02ed13781df7ed8082cb4f7f3b
linuxbandit <bellicaf@tcd.ie>
2023-04-12 14:42:39 +0200

    fix: npx already present, so upgrade npm

--- vagrant-post-script/prerequisites.sh
+++ vagrant-post-script/prerequisites.sh
@@ -3,11 +3,14 @@
 sudo apt-get update
 sudo apt-get install curl -y
 
+NODE_VERSION=16
+NPM_VERSION=9.6.4
+
 echo "[Vagrant] ###################     Installing node tooling for frontend and backend"
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
 export NVM_DIR="$HOME/.nvm"
 # shellcheck disable=SC1090
 [ -s "$NVM_DIR/nvm.sh" ] && source "$NVM_DIR/nvm.sh"
-nvm install 16
-nvm use 16
-sudo npm install -g npx
+nvm install "$NODE_VERSION"
+nvm use "$NODE_VERSION"
+npm install -g npm@"$NPM_VERSION"
