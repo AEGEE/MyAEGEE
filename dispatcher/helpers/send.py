0c8565f75c14be539529b1fc21336fe3fbf5f15e
linuxbandit <bellicaf@tcd.ie>
2023-04-13 22:38:15 +0200

    feat: add new notification dispatcher mechanism

---
+++ dispatcher/helpers/send.py
@@ -0,0 +1,38 @@
+import pika
+import json
+import random
+
+"""
+creates between 1 and 8 fake emails and puts in the queue
+"""
+
+from faker import Faker
+faker = Faker()
+
+RABBIT_HOST='172.18.0.11'
+connection = pika.BlockingConnection(pika.ConnectionParameters(RABBIT_HOST))
+channel = connection.channel()
+
+
+def generate_fake_payload():
+    email = {
+        "from": "mailer@aegee.eu",
+        "to": [faker.email() for _ in range(random.randrange(1,3))],
+        "reply_to": "noreply@aegee.eu",
+        "subject": faker.sentence(),
+        "template": "member_expired",
+        "parameters": {"first_name": faker.name(), "body": f"AEGEE-{faker.city()}", "last_payment": faker.date()}
+    }
+    return email
+
+for _ in range(random.randrange(1,8)):
+    email = generate_fake_payload()
+    channel.queue_declare(queue='email')
+    channel.basic_publish(exchange='',
+                        routing_key='email',
+                        body=json.dumps(email))
+    print(f" [x] Sent {email['subject']} (to {email['to']})")
+
+
+
+connection.close()
