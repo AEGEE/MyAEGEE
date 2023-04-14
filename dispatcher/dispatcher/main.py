67693ffbdd9bde02aff84d5abffdd18b9ec7a51f
linuxbandit <bellicaf@tcd.ie>
2023-04-14 09:57:41 +0200

    feat: add retry if template not found
    removing the auto unack, the program will not remove the message
    from the queue if it can't process it

--- dispatcher/dispatcher/main.py
+++ dispatcher/dispatcher/main.py
@@ -2,7 +2,7 @@ import pika
 import json
 import smtplib
 from email.message import EmailMessage
-from jinja2 import Environment, FileSystemLoader
+from jinja2 import Environment, FileSystemLoader, exceptions
 
 """
 continuously polls the email queue and renders+sends the template on every acked message
@@ -21,18 +21,25 @@ channel.queue_declare(queue='email')
 
 def send_email(ch, method, properties, body):
     msg = json.loads(body)
+    try:
+        template = environment.get_template(f"{msg['template']}.jinja2")
+    except exceptions.TemplateNotFound:
+        return
+    # TODO: check if there is an auto-delete after 30 minutes for stuck un-acked messages
+    # TODO: add auto-retry. rabbit is smart and doesn't let me process a message again unless i force it
+    rendered = template.render(msg['parameters'], altro=msg['subject'])
+
     email = EmailMessage()
+    email.set_content(rendered, subtype='html')
     email['From'] = msg['from']
     email['Reply-To'] = msg['reply_to']
     email['To'] = msg['to']
     email['Subject'] = msg['subject']
-    template = environment.get_template(f"{msg['template']}.jinja2")
-    rendered = template.render(msg['parameters'], altro=msg['subject'])
-    email.set_content(rendered, subtype='html')
     smtpObj.send_message(email)
+    ch.basic_ack(delivery_tag = method.delivery_tag)
 
 channel.basic_consume(queue='email',
-                      auto_ack=True,
+                      auto_ack=False,
                       on_message_callback=send_email)
 
 print(' [*] Waiting for messages. To exit press CTRL+C')
