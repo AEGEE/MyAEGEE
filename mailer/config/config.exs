8f22c92ddc503e817a821d44d330c46b77d4c8e9
Rik Smale <13023439+WikiRik@users.noreply.github.com>
2024-12-06 10:45:36 +0100

    fix: revert env name change (#106)

--- mailer/config/config.exs
+++ mailer/config/config.exs
@@ -18,7 +18,7 @@ end
 # General application configuration
 config :omsmailer,
   ecto_repos: [Omsmailer.Repo],
-  from_address: System.get_env("SMTP_NAME")
+  from_address: System.get_env("SMTP_USER")
 
 # Configures the endpoint
 config :omsmailer, OmsmailerWeb.Endpoint,
