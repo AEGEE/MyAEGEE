d305c3a35ffed6bcf37fe6a123cf047da8752ef9
Rik Smale <13023439+WikiRik@users.noreply.github.com>
2026-01-11 15:19:22 +0100

    fix(su): update for 2026 season (#2513)
    Co-authored-by: Rik Smale <git@riksmale.info>

--- frontend/src/views/summeruniversity/List.vue
+++ frontend/src/views/summeruniversity/List.vue
@@ -34,7 +34,8 @@
                 <option value="2022">2022</option>
                 <option value="2023">2023</option>
                 <option value="2024">2024</option>
-                <option selected value="2025">2025</option>
+                <option value="2025">2025</option>
+                <option selected value="2026">2026</option>
               </select>
             </div>
           </div>
@@ -166,7 +167,7 @@ export default {
       eventTypes,
       eventTypesNames: constants.SUMMERUNIVERSITY_TYPES_NAMES,
       applicationStatuses,
-      season: 2025,
+      season: 2026,
       isLoading: {
         events: false,
         permissions: false
