d1b79c95ab39895e7d7044efaba909ab12b5fc6a
Rik Smale <git@riksmale.info>
2026-02-15 11:59:57 +0100

    fix(statutory): add qs as explicit dependency and fix global require
    Move the inline require('qs') to a top-level import and add qs as an
    explicit dependency in package.json to satisfy eslint's global-require
    and import/no-extraneous-dependencies rules.

--- statutory/lib/server.js
+++ statutory/lib/server.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const router = require('express-promise-router');
 const bodyParser = require('body-parser');
 const boolParser = require('express-query-boolean');
+const qs = require('qs');
 
 const config = require('../config');
 const log = require('./logger');
@@ -41,7 +42,7 @@ const QuestionLinesRouter = router({ mergeParams: true });
 const QuestionsRouter = router({ mergeParams: true });
 
 const server = express();
-server.set('query parser', (str) => require('qs').parse(str, { arrayLimit: 100 }));
+server.set('query parser', (str) => qs.parse(str, { arrayLimit: 100 }));
 server.use(bodyParser.json());
 server.use(boolParser());
 server.use(morgan);
